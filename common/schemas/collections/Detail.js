96b6f305ebb2b1ccfa7e35d200ab8f9c2dfc494d
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2017-11-21 18:12:13 +0900

    commit common

---
+++ common/schemas/collections/Detail.js
@@ -0,0 +1,11 @@
+import Schema from '../Schema';
+
+export default class Detail extends Schema{
+  constructor(){
+    super();
+
+    // Initialize.
+    return this.init({
+    });
+  }
+}
---
+++ common/schemas/state/Index/index.js
@@ -0,0 +1,12 @@
+import Schema from './../../../schemas/Schema';
+
+export default class Index extends Schema{
+  constructor(){
+    super();
+
+    // Initialize.
+    return this.init({
+
+    });
+  }
+}
