4abe673ade12f789cbdeea282ae1637d10f2cea8
color.ui auto <root@ip-172-31-22-183.ap-northeast-1.compute.internal>
2017-11-21 10:09:02 +0000

    server setting

---
+++ server/schemas/state/Thread/Meta.js
@@ -0,0 +1,12 @@
+import Schema from '~/schemas/Schema';
+
+export default class Meta extends Schema{
+  constructor( socket ){
+    super();
+
+    // Initialize.
+    return this.init({
+
+    });
+  }
+}
