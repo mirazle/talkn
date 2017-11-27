0e916b8bac41901dabbbb06b8461e6575cc97790
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2017-11-27 10:44:17 +0900

    BACKUP

--- server/run.js
+++ server/run.js
@@ -21,9 +21,7 @@ class TalknServer{
 	}
 
 	connection( ioUser ){
-    console.log(ioUser.conn.id);
-    console.log(ioUser.handshake.headers.referer);
-/*
+
 		// Generate Redux State Schema .
 		const state = new Schemas.State( ioUser );
 
@@ -34,7 +32,6 @@ class TalknServer{
 		this.oneUserEndpointKeys.forEach( endpointKey => {
 			ioUser.on( endpointKey, ( request ) => Actions[ endpointKey ]( ioUser, state, request ));
 		});
-*/
 	}
 }
 
