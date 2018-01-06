140ce10b08e0e9ed8fba681951f9f1919a5eaf98
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-01-07 00:39:08 +0900

    BACKUP

--- common/schemas/state/User.js
+++ common/schemas/state/User.js
@@ -10,20 +10,21 @@ export default class User extends Schema{
     return this.create({
       id,
       type,
-      inputPost: {value: inputPost, valid: User.validInputPost},
+      inputPost: {value: inputPost},
       inputSearch,
     });
   }
 
   static validInputPost(value){
+    if( /\r\n|\n|\r$/.test( value ) ) return true;
     return false;
   }
 
   static validPost(value){
-    if( value === '' ) return true;
-    if( /^\r\n|\n|\r+$/.test( value ) ) return true;
-    if( /^\r\n|\n|\r+\s+\r\n|\n|\r+$/.test( value ) ) return true;
-    if( /^\r\n|\n|\r+　+\r\n|\n|\r+$/.test( value ) ) return true;
+    if( value === '' ) return 'NO INPUT POST';
+//    if( /^\r\n|\n|\r+$/.test( value ) ) return 'ONLY NEW LINE';
+//    if( /^\r\n|\n|\r+\s+\r\n|\n|\r+$/.test( value ) ) return 'EMPTY INPUT A';
+//    if( /^\r\n|\n|\r+　+\r\n|\n|\r+$/.test( value ) ) return 'EMPTY INPUT B';
     return false;
   }
 }
