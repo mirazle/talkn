06baab0bc7c41e88871693309443d63e384b5b84
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-01-17 22:04:44 +0900

    COMMIT

--- client/reducers/style.js
+++ client/reducers/style.js
@@ -5,7 +5,6 @@ export default ( state = {} , action ) => {
 	switch( action.type ){
 	case 'ON_CLICK_OPEN_THREAD':
 		let transformY = action.user.isDisp ? -Container.headerHeight : Container.threadHeight;
-		console.log( transformY );
 		return {...state,
 			thread: {...state.thread,
 				self: {...state.thread.self,
