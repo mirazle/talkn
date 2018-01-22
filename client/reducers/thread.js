57f205672a51797413a9e29b3b7fae406872fde5
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-01-22 19:42:40 +0900

    COMMIT

--- client/reducers/thread.js
+++ client/reducers/thread.js
@@ -1,5 +1,11 @@
 import Thread from 'common/schemas/state/Thread';
 
 export default ( state = new Thread() , action ) => {
-	return action.thread ? state.merge( action.thread ) : state ;
+	console.log(action.type);
+	switch( action.type ){
+	case 'SERVER_TO_CLIENT[BROADCAST]:post':
+		return state.merge({postCnt: state.postCnt + 1 });
+	default :
+		return action.thread ? state.merge( action.thread ) : state ;
+	}
 };
