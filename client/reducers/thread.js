6964df8743ec24d6057ca14708f504d244f9efc3
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-01-24 15:22:58 +0900

    COMMIT

--- client/reducers/thread.js
+++ client/reducers/thread.js
@@ -5,7 +5,6 @@ export default ( state = new Thread() , action ) => {
 	case 'SERVER_TO_CLIENT[BROADCAST]:post':
 		return state.merge({postCnt: state.postCnt + 1 });
 	default :
-		// TODO serverのthread更新フラグのメソッドはちゃんと機能している？
 		// TODO ブラウザ初期表示時に上に画面を上にスクロールするとスレッドが見えてしまう
 		return action.thread ? state.merge( action.thread ) : state ;
 	}
