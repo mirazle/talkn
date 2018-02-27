d8377ae567417ae8a75bf943b23a9be2b4a19f40
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-02-27 20:21:47 +0900

    COMMIT

--- client/src/talkn.client.js
+++ client/src/talkn.client.js
@@ -6,11 +6,15 @@ import TalknSession from 'client/operations/talknSession';
 import TalknAPI from 'client/operations/talknAPI';
 import TalknViewer from 'client/operations/talknViewer';
 
-function bootTalkn( appType, talknIndex, attributes ){
-	const protcol = location.href.indexOf( 'https' ) === 0 ? 'https' : 'http' ;
-	const server = location.href.indexOf( 'localhost:8080/' ) !== false ? 'localhost' : 'client.talkn.io' ;
-	const port = protcol === 'https' ? 10443 : 10001;
+const env = {};
+env.mode = location.href.indexOf( 'localhost:8080/' ) !== false ? 'DEV' : 'PROD' ;
+env.protcol = location.href.indexOf( 'https' ) === 0 ? 'https' : 'http' ;
+env.server = env.mode === 'DEV' ? 'localhost' : 'client.talkn.io' ;
+env.port = env.protcol === 'https' ? 10443 : 10001;
+env.scriptName = env.mode === 'DEV' ? 'talkn.client.js' : 'client.talkn.io' ;
 
+function bootTalkn( appType, talknIndex, attributes, env ){
+	const {server, port} = env;
 	const ws = io(`//${server}:${port}`, { forceNew: true });
 	const store = configureStore();
 	const state = new State( appType, talknIndex, window, attributes );
@@ -22,8 +26,9 @@ function bootTalkn( appType, talknIndex, attributes ){
 	talknAPI.initClientState( state );
 	talknViewer.render();
 }
-
+console.log("AAA");
 window.onload =  () => {
+console.log("BBB");
 	const appType = TalknViewer.getAppType();
 	window.TalknAPI = TalknAPI;
 	window.__talknAPI__ = [];
@@ -34,9 +39,11 @@ window.onload =  () => {
 		bootTalkn( appType, 0, {} );
 		break;
 	case 'script':
-		const scripts = document.querySelectorAll('script[src*="talkn.client.js"]');
+		const scripts = document.querySelectorAll(`script[src*="${env.scriptName}"]`);
+		console.log( scripts );
+
 		scripts.forEach( ( script, index ) => {
-			bootTalkn( appType, index + 1 , script.attributes );
+			bootTalkn( appType, index + 1 , script.attributes, env );
  		});
 
 		window.talknAPI = window.__talknAPI__[ 1 ];
