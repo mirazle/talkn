c4d201a5f0d0b5a465b6abff4c65f5f29daf47c0
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-03-19 16:49:08 +0900

    COMMIT

--- client/components/Setting.js
+++ client/components/Setting.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from "react"
 import User from 'common/schemas/state/User';
+import SettingLi from 'client/components/SettingLi';
 
 export default class Setting extends Component {
 
@@ -7,6 +8,55 @@ export default class Setting extends Component {
     super(props);
     this.handleOnClickchildrenThreadView = this.handleOnClickchildrenThreadView.bind(this);
     this.handleOnClickLoginTwitter = this.handleOnClickLoginTwitter.bind(this);
+  }
+
+  getDecolationProps(){
+    return {
+      onMouseOver: () => {
+        this.setState(
+          { style:
+            {...this.state.style,
+              self: { ...this.state.style.self,
+                transition: '200ms',
+                transform: 'scale( 1.05 )',
+                cursor: 'pointer',
+              }
+            }
+          }
+        );
+      },
+      onMouseLeave: () => {
+        this.setState( {style:
+          {...this.state.style,
+            self: { ...this.state.style.self,
+              transition: '600ms',
+              transform: 'scale( 1 )',
+              cursor: 'default',
+            }
+          }
+        });
+      },
+      onMouseDown: () => {
+        this.setState( {style:
+          {...this.state.style,
+            self: { ...this.state.style.self,
+              transform: 'scale( 1 )',
+              cursor: 'pointer',
+            }
+          }
+        });
+      },
+      onMouseUp: () => {
+        this.setState( {style:
+          {...this.state.style,
+            self: { ...this.state.style.self,
+              transform: 'scale( 1.05 )',
+              cursor: 'pointer',
+            }
+          }
+        });
+      },
+    }
   }
 
   handleOnClickchildrenThreadView(){
@@ -24,11 +74,19 @@ export default class Setting extends Component {
     talknAPI.login( 'twitter' );
   }
 
+  renderFriendLiLabel( name, icon, connection ){
+    const { style } = this.props.state;
+    return(
+      <div style={ style.setting.wrap }>
+        <img style={ style.setting.img } src={ icon } />
+        <span style={ style.setting.names }>{ name }<br />{ connection }</span>
+      </div>
+    );
+  }
+
  	render() {
     const { style, control, user, thread } = this.props.state;
-    const childrenThreadViewLabel = control.childrenThreadView ? 'ON' : 'OFF';
-
-    const img1 = {...style.setting.img, backgroundImage: 'url( https://pbs.twimg.com/profile_images/927155774937186304/8I_6Wp0c_bigger.jpg )'};
+    const childrenThreadViewLabel = control.childrenThreadView ? 'Children thread view ON' : 'Children thread view OFF';
 
 		return (
       <div style={ style.setting.self } >
@@ -36,105 +94,31 @@ export default class Setting extends Component {
 
           <br />
           <ol style={ style.setting.columns }>
-            <li style={ style.setting.column }>
-              { thread.connection  }
-            </li>
-            <li style={ style.setting.columnLast } onClick={ this.handleOnClickchildrenThreadView }>
-              Children thread view: {childrenThreadViewLabel}
-            </li>
+            <SettingLi label={ thread.connection } isLast={ false } {...this.props.state} />
+            <SettingLi label={ childrenThreadViewLabel } isLast={ true } {...this.props.state} onClick={ this.handleOnClickchildrenThreadView }/>
           </ol>
 
           <br />
           <ol style={ style.setting.columns }>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/1725640801/baba_bigger.png' />
-                <span style={ style.setting.names } >mirazle<br />/news.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/927155774937186304/8I_6Wp0c_bigger.jpg' />
-                <span style={ style.setting.names } >fukuty.sho<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/890932897170898944/yIEPHR9C_bigger.jpg' />
-                <span style={ style.setting.names } >自由になりたいあなたへ<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/946122762396999680/zOCsFkrw_bigger.jpg' />
-                <span style={ style.setting.names } >古谷 沙綾香<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/855356315551449088/UMsYHYKZ_bigger.jpg' />
-                <span style={ style.setting.names } >りゅう＠のんびりパパ<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/589975861878661121/yPshjOKb_bigger.jpg' />
-                <span style={ style.setting.names } >ベンチャータイムス<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/930011731174109184/8FcK1ARX_bigger.jpg' />
-                <span style={ style.setting.names } >中田真広<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/753595099590762497/u3y4bddq_bigger.jpg' />
-                <span style={ style.setting.names } >ｎａｈｏ<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/815378347618205697/d9qS-g6z_bigger.jpg' />
-                <span style={ style.setting.names } >takayukii<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/753735960316121088/8_Pf8eTu_bigger.jpg' />
-                <span style={ style.setting.names } >柳本　顕<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/378800000561520910/39aa6d3e3e119c25136ed1de40705294_bigger.jpeg' />
-                <span style={ style.setting.names } >yukki<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/913411791287345152/BWboXDW2_bigger.jpg' />
-                <span style={ style.setting.names } >さくら剛<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/1519836653/EAAD6F43-D430-476B-916A-2EBF05114616_bigger' />
-                <span style={ style.setting.names } >南野真太郎(MTRo)<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
-            <li style={ style.setting.column }>
-              <div style={ style.setting.wrap }>
-                <img style={ style.setting.img } src='https://pbs.twimg.com/profile_images/27767372/e__-1_bigger.png' />
-                <span style={ style.setting.names } >sk<br />/www.yahoo.co.jp</span>
-              </div>
-            </li>
+            <SettingLi label={ this.renderFriendLiLabel( 'mirazle', 'https://pbs.twimg.com/profile_images/1725640801/baba_bigger.png', '/news.yahoo.co.jp' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'fukuty.sho', 'https://pbs.twimg.com/profile_images/927155774937186304/8I_6Wp0c_bigger.jpg', '/tabelog.com/tokyo' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( '自由になりたいあなたへ', 'https://pbs.twimg.com/profile_images/890932897170898944/yIEPHR9C_bigger.jpg', '/zozo.jp/ranking/all-sales.html' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( '古谷 沙綾香', 'https://pbs.twimg.com/profile_images/946122762396999680/zOCsFkrw_bigger.jpg', '/www.microsoft.com/ja-jp' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'りゅう＠のんびりパパ', 'https://pbs.twimg.com/profile_images/855356315551449088/UMsYHYKZ_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'ベンチャータイムス', 'https://pbs.twimg.com/profile_images/589975861878661121/yPshjOKb_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( '中田真広', 'https://pbs.twimg.com/profile_images/930011731174109184/8FcK1ARX_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'ｎａｈｏ', 'https://pbs.twimg.com/profile_images/753595099590762497/u3y4bddq_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'takayukii', 'https://pbs.twimg.com/profile_images/815378347618205697/d9qS-g6z_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( '柳本　顕', 'https://pbs.twimg.com/profile_images/753735960316121088/8_Pf8eTu_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'yukki', 'https://pbs.twimg.com/profile_images/378800000561520910/39aa6d3e3e119c25136ed1de40705294_bigger.jpeg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'さくら剛', 'https://pbs.twimg.com/profile_images/913411791287345152/BWboXDW2_bigger.jpg' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( '南野真太郎(MTRo)', 'https://pbs.twimg.com/profile_images/1519836653/EAAD6F43-D430-476B-916A-2EBF05114616_bigger' ) } isLast={ false } {...this.props.state} />
+            <SettingLi label={ this.renderFriendLiLabel( 'sk', 'https://pbs.twimg.com/profile_images/27767372/e__-1_bigger.png' ) } isLast={ false } {...this.props.state} />
             <li style={ style.setting.columnLast } onClick={this.handleOnClickLoginTwitter}>
-              → LOGIN
+              ⇨ LOGOUT
             </li>
-            <li style={ style.setting.columnLast } onClick={this.handleOnClickLoginTwitter}>
-              
+            <li style={ style.setting.columnLast } onClick={this.handleOnClickLoginTwitter} >
+
             </li>
           </ol>
           <br />
