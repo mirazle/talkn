4d6e1f3515bf1efb2bd793634c39f37427881b9e
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-03-26 18:49:51 +0900

    COMMIT

--- client/src/talkn.client.js
+++ client/src/talkn.client.js
@@ -30,6 +30,25 @@ window.onload =  () => {
 	const appType = TalknViewer.getAppType();
 	window.TalknAPI = TalknAPI;
 	window.__talknAPI__ = [];
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1655931587827697',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.12'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 	switch( appType ){
 	case 'plugin':
