03afdb61afea57339c74ab5c47a341c201b7932f
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-03-28 21:37:55 +0900

    REMOVE2

--- client/reducers/actionLog.js
+++
@@ -1,1 +0,0 @@
-export default ( state = [], action ) => [ action.type, ...state ];
--- client/reducers/thread.js
+++
@@ -1,5 +0,0 @@
-import Thread from 'common/schemas/state/Thread';
-
-export default ( state = new Thread() , action ) => {
-	return action.thread ? state.merge( action.thread ) : state ;
-};
