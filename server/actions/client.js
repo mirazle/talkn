8400a85cf5f4b588b65e6ca2057e9ab77eb9d5b7
root <root@ip-172-31-27-3.ap-northeast-1.compute.internal>
2018-04-04 08:08:53 +0000

    COMMIT

--- server/actions/client.js
+++ server/actions/client.js
@@ -1,51 +1,23 @@
+import conf from '~/conf';
 import Actions from '~/actions';
 import Logics from '~/logics';
-import utils from '~/utils';
 import https from 'https';
-import fs from 'fs';
 import express from 'express';
-import session from 'express-session';
-import subdomain from 'express-subdomain';
-import subdomainHandler from 'express-subdomain-handler';
-import proxy from 'express-http-proxy';
+import fs from 'fs';
 
-const TALKN_CLIENT_LISTEN_PORT = 443;
-const TALKN_CLIENT_KEY_PEM = '/etc/letsencrypt/live/client.talkn.io/privkey.pem';
-const TALKN_CLIENT_CERT_PEM = '/etc/letsencrypt/live/client.talkn.io/cert.pem';
-
-
-// https://client.talkn.io:8000/
 export default {
   setUpClient: async () => {
 
-/*
-    // Proxy Setting
-    const proxyApp = express();
-    const proxyRouter = express.Router();
-    proxyApp.use( subdomain('client', proxyRouter) );
-    proxyApp.use('/', proxy('talkn.io:8000') );
-
-    const proxyOptions = {key:  fs.readFileSync( TALKN_CLIENT_KEY_PEM ), cert: fs.readFileSync( TALKN_CLIENT_CERT_PEM )};
-    const proxyServer = https.createServer( proxyOptions, proxyApp );
-    proxyServer.listen( TALKN_CLIENT_LISTEN_PORT, () => {
-       console.log( `PROXY CLIENT ${TALKN_CLIENT_LISTEN_PORT}` );
-    });
-
-    // Server Setting
     const app = express();
-    app.get('/', function(req, res) {
-      console.log("TOP");
-      const fileName = 'talkn.client.js'; 
-      const path = `${__dirname}/../endpoints/client/`; 
-      console.log( path + fileName );
 
-      res.send("HELLO!CLIENT!");
-      //res.download( path, fileName );
+    app.get('*', (req, res) => {
+      res.sendFile( '/usr/share/app/talkn/server/endpoints/client/talkn.client.js');
+      return true;
     });
-    app.listen( 8000, () => {
-       console.log( `LISTEN CLIENT 8000` );
+
+    app.listen( 8001, () => {
+       console.log( `LISTEN CLIENT 8001` );
     });
-*/
     return true;  
   },
 }
--- server/conf/index.js
+++ server/conf/index.js
@@ -1,15 +1,26 @@
+import fs from 'fs';
+
 const PRODUCTION = 'production';
 const DEVELOPMENT = 'development';
 const env = process.env.HOSTNAME ? PRODUCTION : DEVELOPMENT ;
+
 const proxySllKeyPem = env === PRODUCTION ? '/etc/letsencrypt/live/talkn.io-0001/privkey.pem' : ''; 
 const proxySllCertPem = env === PRODUCTION ? '/etc/letsencrypt/live/talkn.io-0001/cert.pem' : ''; 
+const proxySllPems = env === PRODUCTION ? 
+  {key:  fs.readFileSync( proxySllKeyPem ), cert: fs.readFileSync( proxySllCertPem )} :
+  {key:  fs.readFileSync( proxySllKeyPem ), cert: fs.readFileSync( proxySllCertPem )} ;
 
+
+const sllKeyPem = env === PRODUCTION ? '/etc/letsencrypt/live/talkn.io/privkey.pem' : ''; 
+const sllCertPem = env === PRODUCTION ? '/etc/letsencrypt/live/talkn.io/cert.pem' : ''; 
+const sllPems = env === PRODUCTION ? 
+  {key:  fs.readFileSync( sllKeyPem ), cert: fs.readFileSync( sllCertPem )} :
+  {key:  fs.readFileSync( sllKeyPem ), cert: fs.readFileSync( sllCertPem )} ;
 
 export default {
   env,
-  proxySllPort: 443,
-  proxySllKeyPem,
-  proxySllCertPem,
+  sllOptions:{ pems: sllPems, port: 443 },
+  proxySllOptions:{ pems: proxySllPems, port: 443 },
   user: {},
   client: {sync_time: 500, stream_sync_term: 12,stream_sync_term_cnt: 12},
   ssl: { key: '/etc/letsencrypt/live/discovery-plus.com/cert.pem', cert: '/etc/letsencrypt/live/discovery-plus.com/privkey.pem' },
