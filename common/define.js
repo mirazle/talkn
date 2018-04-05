8b58457525d588e1e299fd85c6a52988ec33eac3
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-04-05 20:32:32 +0900

    COMMIT

---
+++ common/define.js
@@ -0,0 +1,7 @@
+export default {
+  PRODUCTION:'production',
+  DEVELOPMENT: 'development',
+  PRODUCTION_IP: 'ip-172-31-27-3',
+  PRODUCTION_DOMAIN: 'talkn.io',
+  DEVELOPMENT_DOMAIN: 'localhost',
+}
