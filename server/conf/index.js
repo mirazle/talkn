2fab33b019aa67fd2eb087263368c7ec941abadf
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-04-05 18:36:16 +0900

    COMMIT

--- server/conf/index.js
+++ server/conf/index.js
@@ -1,6 +1,8 @@
 import conf from '../../common/conf';
 import fs from 'fs';
 
+
+console.log( process.env );
 conf.sllOptions.pems.key = fs.readFileSync( conf.sllOptions.pems.key );
 conf.sllOptions.pems.cert = fs.readFileSync( conf.sllOptions.pems.cert );
 conf.proxySllOptions.pems.key = fs.readFileSync( conf.proxySllOptions.pems.key );
