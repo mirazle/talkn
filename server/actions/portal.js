457601189fb125bb778ee744c8434df5bbcf5dbe
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-04-06 13:01:11 +0900

    COMMIT

--- server/actions/portal.js
+++ server/actions/portal.js
@@ -2,22 +2,27 @@ import conf from '~/conf';
 import Actions from '~/actions';
 import Logics from '~/logics';
 import express from 'express';
+import detect from 'detect-port';
 
 export default {
 
   setUpPortal: async () => {
+    const port = 8000;
+    detect(port, (err, _port) => {
+      if (!err) {
+        const app = express();
+        app.set('views', `${__dirname}/../endpoints/portal/ejs/`);
+        app.set('view engine', 'ejs');
+        app.get('*', function(req, res) {
+          const params = Object.values( req.params )[ 0 ];
+          res.render('index', {title : params, clientSrcPath: conf.clientSrcPath});
+        });
 
-    const app = express();
-    app.set('views', `${__dirname}/../endpoints/portal/ejs/`);
-    app.set('view engine', 'ejs');
-    app.get('*', function(req, res) {
-      const params = Object.values( req.params )[ 0 ];
-      res.render('index', {title : params, clientSrcPath: conf.clientSrcPath});
+        app.listen( port, () => {
+          console.log( `LISTEN PORTAL 8000` );
+        } );
+      }
     });
-
-    app.listen( 8000, () => {
-      console.log( `LISTEN PORTAL 8000` );
-    } );
     return true;
   },
 }
