c6b9334358128a82fb3c50f917a574cad806b5ce
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-05-01 15:42:43 +0900

    COMMIT

--- client/components/Menu.js
+++ client/components/Menu.js
@@ -118,14 +118,13 @@ export default class Menu extends Component {
 
           <br />
           <ol style={ style.menu.columns }>
-{ /*
+
             <MenuLi label={ this.renderFriendLiLabel( 'mirazle', 'https://pbs.twimg.com/profile_images/1725640801/baba_bigger.png', '/news.yahoo.co.jp' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'fukuty.sho', 'https://pbs.twimg.com/profile_images/927155774937186304/8I_6Wp0c_bigger.jpg', '/twitter.com/fukutys' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( '自由になりたいあなたへ', 'https://pbs.twimg.com/profile_images/890932897170898944/yIEPHR9C_bigger.jpg', '/zozo.jp/ranking/all-sales.html' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( '古谷 沙綾香', 'https://pbs.twimg.com/profile_images/946122762396999680/zOCsFkrw_bigger.jpg', '/www.microsoft.com/ja-jp' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'りゅう＠のんびりパパ', 'https://pbs.twimg.com/profile_images/855356315551449088/UMsYHYKZ_bigger.jpg' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'ベンチャータイムス', 'https://pbs.twimg.com/profile_images/589975861878661121/yPshjOKb_bigger.jpg' ) } isLast={ false } {...this.props.state} />
-            <MenuLi label={ this.renderFriendLiLabel( '中田真広', 'https://pbs.twimg.com/profile_images/930011731174109184/8FcK1ARX_bigger.jpg' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'ｎａｈｏ', 'https://pbs.twimg.com/profile_images/753595099590762497/u3y4bddq_bigger.jpg' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'takayukii', 'https://pbs.twimg.com/profile_images/815378347618205697/d9qS-g6z_bigger.jpg' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( '柳本　顕', 'https://pbs.twimg.com/profile_images/753735960316121088/8_Pf8eTu_bigger.jpg' ) } isLast={ false } {...this.props.state} />
@@ -133,7 +132,7 @@ export default class Menu extends Component {
             <MenuLi label={ this.renderFriendLiLabel( 'さくら剛', 'https://pbs.twimg.com/profile_images/913411791287345152/BWboXDW2_bigger.jpg' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( '南野真太郎(MTRo)', 'https://pbs.twimg.com/profile_images/1519836653/EAAD6F43-D430-476B-916A-2EBF05114616_bigger' ) } isLast={ false } {...this.props.state} />
             <MenuLi label={ this.renderFriendLiLabel( 'sk', 'https://pbs.twimg.com/profile_images/27767372/e__-1_bigger.png' ) } isLast={ false } {...this.props.state} />
-*/}
+
             <MenuLi label={ '⇨ LOGIN FACEBOOK' }  isLast={ false }onClick={this.handleOnClickLoginFacebook}  {...this.props.state} />
             <MenuLi label={ '⇨ LOGIN TWITTER' }  isLast={ true }onClick={this.handleOnClickLoginTwitter}  {...this.props.state} />
           </ol>
