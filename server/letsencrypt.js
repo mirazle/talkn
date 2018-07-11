4f0b945e8d117fe9d0439dba39e57f1dcf3d448e
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-07-11 17:25:45 +0900

    COMMIT

--- server/letsencrypt.js
+++ server/letsencrypt.js
@@ -1,15 +1,8 @@
-/**************************************/
-/*  talknServer
-/* ( SOCKET.IO / MONGODB / REDIS )
-/**************************************/
-
 var http = require('http')
 var express = require('express')
-
-const httpApp = express();
+var httpApp = express();
 
 http.createServer(( httpApp ).all( "*", ( req, res ) => {
-  console.log( req.url );
   res.send("csEKOPF59h1smy-fGXqqzP5yFHfepwT0ANUP4Drvk-0.8jGwdr4wmDIONjqGNuTy8tOwF-c71GdlnOwlOLRRAHs");
 })).listen( 80, () => {
   console.log("Listen 80");
