a09b84bcd752d7997bd6e39b9267eb6b0047381b
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-07-28 14:43:21 +0900

    Refact server code.

--- server/actions/index.js
+++ server/actions/index.js
@@ -2,8 +2,8 @@ import db from '~/server/actions/db';
 import io from '~/server/actions/io';
 import express from '~/server/actions/express';
 
-let actions = {};
-Object.keys( db ).forEach( funcName => actions[ funcName ] = db[ funcName ]);
-Object.keys( io ).forEach( funcName => actions[ funcName ] = io[ funcName ]);
-Object.keys( express ).forEach( funcName => actions[ funcName ] = express[ funcName ]);
+let actions = { db: {}, io: {}, express: {} };
+Object.keys( db ).forEach( funcName => actions.db[ funcName ] = db[ funcName ]);
+Object.keys( io ).forEach( funcName => actions.io[ funcName ] = io[ funcName ]);
+Object.keys( express ).forEach( funcName => actions.express[ funcName ] = express[ funcName ]);
 export default actions;
