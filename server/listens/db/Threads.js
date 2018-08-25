4dc06eae8169b2c247457897e2cc58f658af7054
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-08-25 14:54:25 +0900

    Rename that Mongo access method name.

--- server/listens/db/Threads.js
+++ server/listens/db/Threads.js
@@ -22,7 +22,7 @@ export default class Threads {
 
   update( condition = {}, set = {}, option = {} ){
     return new Promise( resolve => {
-      this.collection.update( condition, set, option, ( error, response ) => {
+      this.collection.updateMany( condition, set, option, ( error, response ) => {
         if(error) console.warn( error );
         resolve({response, error});
       });
