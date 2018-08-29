4682075aa765dfaa2d754ad72102694432074eef
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-08-29 22:01:35 +0900

    Resolved Issue/24

--- client/operations/TalknAPI.js
+++ client/operations/TalknAPI.js
@@ -104,7 +104,6 @@ export default class TalknAPI{
 			if( TalknAPI.handle( talknIndex ) ){
 				const reduxState = window.talknAPI.store.getState();
 				const requestState = Sequence.getRequestState( actionName, reduxState, requestParams );
-console.log( requestState );
 				const actionState = Sequence.getRequestActionState( actionName, requestParams );
 				this.ws.emit( requestState.type, requestState );
 				return window.talknAPI.store.dispatch( actionState );
@@ -125,7 +124,6 @@ console.log( requestState );
 		return ( response ) => {
 			if( TalknAPI.handle( talknIndex ) ){
 				const actionState = actionMethod( response );
-//				console.log( "=== getCatchConnectionAPI === " + actionState.type);
 				return window.talknAPI.store.dispatch( actionState );
 			}
 		}
