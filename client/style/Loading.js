db4af6924d2fcf30b588e0f940ba3aca5fce0fe5
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-09-01 23:37:00 +0900

    Resolved this branch

---
+++ client/style/Loading.js
@@ -0,0 +1,24 @@
+import App from '../../common/schemas/state/App';
+import Style from './index';
+import Container from './Container';
+
+
+export default class Loading{
+  constructor( params ){
+    const self = Loading.getSelf( params );
+    return {
+      self
+    }
+  }
+
+  static getSelf(){
+    const layout = Style.getLayoutFlex({
+      margin: '0 auto',
+      height: '100%',
+      width: '13%',
+    });
+    const content = {};
+    const animation = Style.getAnimationBase();
+    return Style.get({layout, content, animation});
+  }
+}
