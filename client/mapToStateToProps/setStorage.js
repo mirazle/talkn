29aa785bd21b927a2acd9e6517fda86a90be2f69
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-10-30 21:17:13 +0900

    #63 [Phase1]ブラウザのタブ毎にlocalstorageを分ける

---
+++ client/mapToStateToProps/setStorage.js
@@ -0,0 +1,44 @@
+import define from 'common/define';
+import TalknSession from 'client/operations/TalknSession';
+
+export default {
+  "SERVER_TO_CLIENT[EMIT]:initClientState" : ( state, props ) => {
+    const { storageKey } = define;
+    const { rootConnection } = state.app;
+    TalknSession.setStorage( rootConnection, define.storageKey[ storageKey.postSingle ], [] );
+    TalknSession.setStorage( rootConnection, define.storageKey[ storageKey.postMulti ], [] );
+    return {state, props};
+  },
+  "SERVER_TO_CLIENT[EMIT]:getMore": setStorages,
+  "SERVER_TO_CLIENT[EMIT]:find": setStorages,
+  "SERVER_TO_CLIENT[EMIT]:changeThread": ( state, props ) => {
+    const { app, setting } = state;
+    const { rootConnection } = app;
+    const { storageKey } = define;
+    const removePostKey = setting.multistream ? storageKey.postSingle : storageKey.postMulti ;
+    TalknSession.setStorage( rootConnection, define.storageKey[ removePostKey ], [] );
+    return {state, props};
+  },
+  "UPDATE_SETTING": ( state, props ) => {
+    const { rootConnection } = state.app;
+    TalknSession.setStorage( rootConnection, define.storageKey.updateSetting, state.setting.toJSON() );
+    return {state, props};
+  },
+  "ON_CLICK_MENU": ( state, props ) => {
+    const { rootConnection } = state.app;
+
+    TalknSession.setStorage( rootConnection, define.storageKey.selectMenu, state.app.menuComponent );
+    return {state, props};
+  },
+}
+
+function setStorages( state, props ){
+  const { app, setting } = state;
+  const { rootConnection } = app;
+  const { storageKey } = define;
+  const getPostKey = setting.multistream ? storageKey.postMulti : storageKey.postSingle ;
+
+  TalknSession.setStorage( rootConnection, define.storageKey.menuLogs, state.menuLogs.toJSON() );
+  TalknSession.setStorage( rootConnection, define.storageKey[ getPostKey ], state.posts );
+  return {state, props};
+}
