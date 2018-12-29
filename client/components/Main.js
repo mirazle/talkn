37395ef1a7e22ccb99fc1a7469cd537894c60061
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2018-12-29 16:42:51 +0900

    Update Notif

--- client/components/Main.js
+++ client/components/Main.js
@@ -2,17 +2,43 @@ import define from 'common/define';
 import React, { Component } from "react"
 import Screen from './Screen';
 import Header from './Header';
+import Notif from './Notif';
 
 export default class Main extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      notifs: [],
+    };
+  }
+
+  componentDidUpdate(){
+    const { actionLog, posts } = this.props.state;
+    switch( actionLog[ 0 ] ){
+    case 'SERVER_TO_CLIENT[BROADCAST]:post':
+      const latPost = posts[posts.length - 1];
+      this.setState({
+        notifs: this.state.notifs.concat(
+          <Notif key={latPost._id} {...latPost} />
+        )
+      });
+      break;
+    }
   }
 
   renderHeader(){
     const{ app } = this.props.state;
     return app.type === define.APP_TYPES.EXTENSION ?
       null : <Header {...this.props} />;
+  }
+
+  renderScreen(){
+    return <Screen {...this.props} />;
+  }
+
+  renderNotif(){
+    return <ol>{this.state.notifs}</ol>;
   }
 
  	render() {
@@ -22,7 +48,8 @@ export default class Main extends Component {
     return (
       <main data-component-name={this.constructor.name} style={ style.main.self }>
         {this.renderHeader()}
-        <Screen {...this.props} />
+        {this.renderScreen()}
+        {this.renderNotif()}
       </main>
 		);
  	}
