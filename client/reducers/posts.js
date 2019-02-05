fd02853cbacbdd3a4c0f5f77584fc03dc0fcca92
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-02-05 22:26:04 +0900

    Update Prod

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,8 +1,5 @@
 import Posts from 'common/schemas/state/Posts';
 
-// get moreしたら一番下までスクロールしてしまっている
-// Multiなのに、Singleが表示されている。且つ、getMoreがループする(判定がおかしい)
-
 export default ( state = new Posts() , action ) => {
 	return state;
 };
