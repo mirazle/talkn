d63934c3c9255cf877645bbba138421e434ad24b
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-02-08 21:36:50 +0900

    exeDeployCommand

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 [ $1 ] && comment=$1 || comment="exeDeployCommand"
 
-echo "@@@@@@ GIT PULL @@@@@@"
+echo "@@@@@@ GIT PULL"
 git pull
-echo "@@@@@@ YARN RUN BUILD @@@@@@"
+echo "@@@@@@ YARN RUN BUILD"
 yarn run build
-echo "@@@@@@ RESOLVE EXTENSION @@@@@@"
+echo "@@@@@@ RESOLVE EXTENSION"
 cp server/listens/express/talkn.client.js extention/talkn.client.js
 zip chromeExtension extention/*
-echo "@@@@@@ GIT PUSH @@@@@@"
+echo "@@@@@@ GIT PUSH"
 git add ./*
-git commit -m comment
+git commit -m $comment
 git push
