853ea297594c638c898679cd1ebce7fe43ab735f
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-02-08 21:31:10 +0900

    comment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+[ $1 ] && comment=$1 || comment="exeDeployCommand"
+
 git pull
 yarn run build
 cp server/listens/express/talkn.client.js extention/talkn.client.js
 zip chromeExtension extention/*
 git add ./*
-git commit -m "deploy.sh"
+git commit -m comment
 git push
