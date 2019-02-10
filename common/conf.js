6b3a48c35542cd3b361e4e6ef0311149cad8afb6
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-02-10 12:53:37 +0900

    Execute deploy command

--- common/conf.js
+++ common/conf.js
@@ -3,11 +3,6 @@ import os from 'os';
 const { PRODUCTION, DEVELOPMENT, PRODUCTION_IP, PRODUCTION_DOMAIN, DEVELOPMENT_DOMAIN, SUB_DOMAINS } = define;
 const hostName = os.hostname();
 const env = hostName === PRODUCTION_IP || hostName.indexOf( PRODUCTION_DOMAIN ) >= 0 ? PRODUCTION : DEVELOPMENT ;
-
-console.log(env);
-console.log(PRODUCTION_IP);
-console.log(hostName);
-
 const domain = env === PRODUCTION ? PRODUCTION_DOMAIN : DEVELOPMENT_DOMAIN ;
 const portalURL = `${SUB_DOMAINS.PORTAL}.${domain}` ;
 const clientURL = `${SUB_DOMAINS.CLIENT}.${domain}` ;
