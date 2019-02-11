f6131b21bb436c76ae568ffcd7303e1e47392097
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-02-11 14:27:42 +0900

    Execute deploy command

--- client/components/Style.js
+++ client/components/Style.js
@@ -5,15 +5,15 @@ export default class  Style extends Component {
  	render() {
 		const{ state, talknAPI } = this.props;
     const { app, style } = state;  
-    const fontSize = app.width <= 290 ? 10 : 12;
+    const fontSize = app.width <= 300 ? 10 : 12;
 
     return (
       <style type='text/css'>
         {`
           #talkn1 textarea::placeholder {
-            text-indent: 2% !important;
-            font-size: ${fontSize}px !important;
-            color: rgb(170, 170, 170) !important;
+            text-indent: 2%;
+            font-size: ${fontSize}px;
+            color: rgb(170, 170, 170);
           }
         `}
       </style>
