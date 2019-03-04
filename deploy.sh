d63a339c936fad61a5550b3d844beec3a0e2a1a7
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-03-04 13:52:24 +0900

    Execute deploy command

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git pull
 echo "@@@@@@ YARN RUN BUILD"
 yarn run build
 echo "@@@@@@ RESOLVE EXTENSION"
-cp server/listens/express/talkn.client.js extention/talkn.client.js
+cp server/listens/express/client/talkn.client.js extention/talkn.client.js
 zip chromeExtension extention/*
 echo "@@@@@@ GIT PUSH"
 git add ./*
