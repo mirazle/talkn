0c3ff796f11b81c8822f4d9c6389c163e7691fdf
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-03-19 16:33:03 +0900

    Execute deploy command

--- client/Container.js
+++ client/Container.js
@@ -5,9 +5,11 @@ import App from 'common/schemas/state/App';
 import Loading from 'client/components/Loading';
 import Style from 'client/components/Style';
 import Main from 'client/components/Main';
+import Posts from 'client/components/Posts';
 import handles from 'client/actions/handles';
 import callbacks from 'client/actions/callbacks';
-import Footer from 'client/components/Footer';
+import Footer from 'client/components/Header';
+import Header from 'client/components/Header';
 import mapToStateToProps from 'client/mapToStateToProps/';
 
 class Container extends Component {
@@ -18,6 +20,9 @@ class Container extends Component {
     talknAPI.find( thread.connection );
     talknAPI.findMenuIndex( thread.connection );
     this.getProps = this.getProps.bind(this);
+    this.renderSmall = this.renderSmall.bind(this);
+    this.renderMiddle = this.renderMiddle.bind(this);
+    this.renderLarge = this.renderLarge.bind(this);
     this.handleOnClickToggleMain = this.handleOnClickToggleMain.bind(this);
     this.handleOnClickToggleDetail = this.handleOnClickToggleDetail.bind(this);
   }
@@ -71,31 +76,60 @@ class Container extends Component {
     }
   }
 
-  renderFooter(){
-    const { app } = this.props.state;
-    if( app.type ===  define.APP_TYPES.EXTENSION){
-      return <Footer {...this.getProps()} />;
-    }else{
-      switch( app.screenMode ){
-      case App.screenModeSmallLabel :
-        return null;
-      case App.screenModeMiddleLabel : 
-      case App.screenModeLargeLabel : 
-      return <Footer {...this.getProps()} />;
-      }
-    }
+  renderLarge(){
+    const { style } = this.props.state;
+    return (
+      <div data-component-type={this.constructor.name} style={ style.container.self }>
+        <Style {...this.getProps()} />
+        <Main {...this.getProps()} />
+        <Footer {...this.getProps()} />
+      </div>
+    );
+  }
+
+  renderMiddle(){
+    const { style } = this.props.state;
+    return (
+      <div data-component-type={this.constructor.name} style={ style.container.self }>
+        <Style {...this.getProps()} />
+        <Main {...this.getProps()} />
+        <Footer {...this.getProps()} />
+      </div>
+    );
+  }
+
+  renderSmall(){
+    const { style } = this.props.state;
+    return (
+      <div data-component-type={this.constructor.name} style={ style.container.self }>
+        <Style {...this.getProps()} />
+        <Posts {...this.getProps()} />
+      </div>
+    );
+  }
+
+  renderExtension(){
+    const { style } = this.props.state;
+    return (
+      <div data-component-type={this.constructor.name} style={ style.container.self }>
+        <Style {...this.getProps()} />
+        <Main {...this.getProps()} />
+        <Footer {...this.getProps()} />
+      </div>
+    );
   }
 
  	render() {
     const { style, app } = this.props.state;
     if( style && style.container && style.container.self && app.connectioned ){
-      return (
-  			<div data-component-type={this.constructor.name} style={ style.container.self }>
-          <Style {...this.getProps()} />
-          <Main {...this.getProps()} />
-          {this.renderFooter()}
-  			</div>
-  		);
+      switch( app.screenMode ){
+      case App.screenModeSmallLabel :
+        return this.renderSmall(this);
+      case App.screenModeMiddleLabel : 
+        return this.renderMiddle(this);
+      case App.screenModeLargeLabel : 
+        return this.renderLarge(this);
+      }
     }else{
       return <Loading />;
     }
