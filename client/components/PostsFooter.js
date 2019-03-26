1e1e5bd408f4b16dc0bd9cf24b6dc1b519ee93da
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-03-26 22:11:43 +0900

    Execute deploy command

--- client/components/PostsFooter.js
+++ client/components/PostsFooter.js
@@ -31,6 +31,7 @@ export default class PostsFooter extends Component {
       const { app } = this.props.state;
       if( app.screenMode === App.screenModeSmallLabel ){
         if( !talknWindow.isScrollBottom ){
+          alert("SCROLL");
           window.scrollTo(0, talknWindow.threadHeight);    
         }
       }
