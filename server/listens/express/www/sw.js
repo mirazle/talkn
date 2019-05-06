fc914626f976164bc9a5ddb3d33d7de9bd20c661
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-05-06 16:45:28 +0900

    Execute deploy command

--- server/listens/express/www/sw.js
+++ server/listens/express/www/sw.js
@@ -4,16 +4,16 @@ const filesToCache = [
     '/',
     '/index.ejs'
 ]
-console.log("SW");
+//console.log("SW");
 // Install Service Worker
 self.addEventListener('install', function(event){
-    console.log('[INSTALL]Service Worker: Installing.... ' + cacheName);
+//    console.log('[INSTALL]Service Worker: Installing.... ' + cacheName);
 
     event.waitUntil(
 
         // Open the Cache
         caches.open(cacheName).then(function(cache) {
-            console.log('[INSTALLED]Service Worker: Caching App Shell at the moment...... ' + cacheName);
+//            console.log('[INSTALLED]Service Worker: Caching App Shell at the moment...... ' + cacheName);
 
             // Add Files to the Cache
             return cache.addAll(filesToCache);
@@ -26,16 +26,16 @@ self.addEventListener('activate', function(event) {
 
     //console.log('Service Worker: Activating....');
     var cacheWhitelist = ['dependencies-cache-**v1**', 'dependencies-2-cache-**v1**'];// Version for your cache list 
-console.log("A");
+//console.log("A");
     event.waitUntil(
         caches.keys().then( (cacheNames) => {
-            console.log("B");
+//            console.log("B");
             return Promise.all(
 
                 cacheNames.map(function(cacheName) {
-                    console.log("C");
+//                    console.log("C");
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        console.log("D");
+//                        console.log("D");
                         return caches.delete(cacheName);
                     }
                 })
@@ -45,20 +45,14 @@ console.log("A");
     return self.clients.claim();
 });
 
-if( self.clients && self.clients.claim ){
-    console.log("E");
-    //self.clients.claim()
-}
-
 self.addEventListener('fetch', function(event) {
 
-    console.log('Service Worker: Fetch', event.request.url);
-
-    console.log("Url", event.request.url);
+//    console.log('Service Worker: Fetch', event.request.url);
+//    console.log("Url", event.request.url);
 
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            console.log("F");
+//            console.log("F");
             return response || fetch(event.request);
         })
     );
