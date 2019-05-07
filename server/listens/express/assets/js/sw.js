9ca936609b95e3abae0f716fc2b11cbc62d0e70b
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-05-07 21:37:25 +0900

    Execute deploy command

--- server/listens/express/assets/js/sw.js
+++ server/listens/express/assets/js/sw.js
@@ -85,10 +85,8 @@ class ServiceWorker {
 
     loadIframe(e){
         alert("LOAD");
-        alert( e.path );
-        alert( e.path[0] );
-        alert( e.path[1] );
-        this.iframe = e.path[1].querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+//        this.iframe = e.path[1].querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+        this.iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
         this.postMessage("bootExtension");
     }
 
