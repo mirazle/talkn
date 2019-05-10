fdc1c7618a624445849f67b182356da2a7bf0505
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-05-10 14:16:36 +0900

    Execute deploy command

--- server/listens/express/assets/js/ext.js
+++ server/listens/express/assets/js/ext.js
@@ -1,42 +1,45 @@
-class ServiceWorker {
+class Ext {
 
     static get MODE(){return "SCRIPT"}
     static get APP_NAME(){return "talkn"}
-    static get ENV(){return "PROD"}
+    static get ENV(){
+        if( location.host.indexOf( Ext.BASE_DEV_HOST) >= 0 ) return "DEV";
+        return "PROD";
+    }
     static get PROTOCOL(){return "https"}
     static get BASE_PROD_HOST(){return "talkn.io"}
     static get BASE_DEV_HOST(){return "localhost"}
     static get BASE_DEV_PORT(){return 8080} 
     static get EXCLUSION_HOSTS(){return ['localhost', 'talkn.io']}    
     static get BASE_HOSTNAME(){
-        if(ServiceWorker.ENV === "PROD"){
-            return `${ServiceWorker.PROTOCOL}://${ServiceWorker.BASE_PROD_HOST}`;
-        }else if(ServiceWorker.ENV === "START"){
-            return `${ServiceWorker.PROTOCOL}://${ServiceWorker.BASE_DEV_HOST}`;
-        }else if(ServiceWorker.ENV === "DEV"){
-            return `${ServiceWorker.PROTOCOL}://${ServiceWorker.BASE_DEV_HOST}:${ServiceWorker.BASE_DEV_PORT}`;
+        if(Ext.ENV === "PROD"){
+            return `${Ext.PROTOCOL}://${Ext.BASE_PROD_HOST}`;
+        }else if(Ext.ENV === "START"){
+            return `${Ext.PROTOCOL}://${Ext.BASE_DEV_HOST}`;
+        }else if(Ext.ENV === "DEV"){
+            return `${Ext.PROTOCOL}://${Ext.BASE_DEV_HOST}:${Ext.BASE_DEV_PORT}`;
         }
     };
     static get iframeCloseHeight(){return '45px'};
     static get iframeCloseNotifHeight(){return '85px'};
     static get iframeOpenHeight(){
-        return ServiceWorker.MODE === "SCRIPT" ?
+        return Ext.MODE === "SCRIPT" ?
             `${Math.floor( window.innerHeight * 0.9 )}px` : '450px';
     };
     static get talknNotifId(){return "talknNotifId"};
     static get activeMethodSecond(){return 1000};
-    static get aacceptPostMessages(){return ['toggleIframe', 'location', 'openNotif', 'closeNotif', 'linkTo', 'getClientMetas']};
+    static get aacceptPostMessages(){return ['toggleExt', 'location', 'openNotif', 'closeNotif', 'linkTo', 'getClientMetas']};
 
     constructor(refusedFrame = false){
         this.connection = location.href.replace("http:/", "").replace("https:/", "");
         const hasSlash = this.connection.lastIndexOf("/") === ( this.connection.length - 1 );
         this.connection = hasSlash ? this.connection : this.connection + "/";
-        const noBootFlg = ServiceWorker.EXCLUSION_HOSTS.some( ( host ) =>{
+        const noBootFlg = Ext.EXCLUSION_HOSTS.some( ( host ) =>{
             this.connection.indexOf(host) >= 0
         });
 
         if(!noBootFlg){
-            const talknFrame = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+            const talknFrame = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
             if( refusedFrame && talknFrame !== null){
                 talknFrame.remove();
             }
@@ -46,7 +49,7 @@ class ServiceWorker {
             this.bootExtension = this.bootExtension.bind(this);
             this.catchMessage = this.catchMessage.bind(this);
             this.handleErrorMessage = this.handleErrorMessage.bind(this);
-            this.toggleIframe = this.toggleIframe.bind(this);
+            this.toggleExt = this.toggleExt.bind(this);
             this.location = this.location.bind(this);
             this.openNotif = this.openNotif.bind(this);
             this.closeNotif = this.closeNotif.bind(this);
@@ -55,10 +58,10 @@ class ServiceWorker {
             // setupWindow
             this.setupWindow();
             this.iframe  = document.createElement("iframe");
-            this.loadIframe = this.loadIframe.bind(this);
+            this.loadExt = this.loadExt.bind(this);
             this.talknUrl = refusedFrame ?
-                chrome.runtime.getURL('index.html?' + this.connection) : ServiceWorker.BASE_HOSTNAME + this.connection;
-            this.iframe.setAttribute("id", `${ServiceWorker.APP_NAME}Extension`);
+                chrome.runtime.getURL('index.html?' + this.connection) : Ext.BASE_HOSTNAME + this.connection;
+            this.iframe.setAttribute("id", `${Ext.APP_NAME}Extension`);
             this.iframe.setAttribute("name", "extension");
             this.iframe.setAttribute("style",
                 "z-index: 2147483647 !important;" +
@@ -68,7 +71,7 @@ class ServiceWorker {
                 "bottom: 0px !important;" + 
                 "right: 0px !important;" + 
                 "width: 100% !important;" + 
-                `height: ${ServiceWorker.iframeCloseHeight} !important;` + 
+                `height: ${Ext.iframeCloseHeight} !important;` + 
                 "margin: 0 !important;" + 
                 "padding: 0!important;" + 
                 "transition: 0ms !important;" + 
@@ -76,7 +79,7 @@ class ServiceWorker {
             );
             this.iframe.setAttribute("src", this.talknUrl );
             this.iframe.setAttribute("frameBorder", 0 );
-            this.iframe.addEventListener( "load", this.loadIframe );
+            this.iframe.addEventListener( "load", this.loadExt );
             this.iframe.addEventListener( "transitionend", this.transitionend );
 
 //            document.querySelector("#talknTest").appendChild(this.iframe);
@@ -88,13 +91,13 @@ class ServiceWorker {
         window.addEventListener('message', this.catchMessage, false);
     }
 
-    loadIframe(e){
-        this.iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
-        this.postMessage("bootExtension", {extensionMode: ServiceWorker.MODE});
+    loadExt(e){
+        this.iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
+        this.postMessage("bootExtension", {extensionMode: Ext.MODE});
     }
 
     transitionend(e){
-        const iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+        const iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
         iframe.style.transition = "0ms";
         iframe.backgroundColor = "green";
         // TODO onTransitionしないとdetail開くときにアニメーションにならない。
@@ -103,17 +106,17 @@ class ServiceWorker {
     }
 
     bootExtension(params){
-        const iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
-        iframe.style.height = ServiceWorker.iframeCloseHeight;
+        const iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
+        iframe.style.height = Ext.iframeCloseHeight;
         iframe.style.display = "flex";
         this.postMessage("offTransition");
     }
 
     catchMessage(e){
         const {type, method, params} = e.data;
-        if( type === ServiceWorker.APP_NAME ){
+        if( type === Ext.APP_NAME ){
             if(this[ method ] && typeof this[ method ] === "function"){
-                if(this.methodIdMap[ method ] || ServiceWorker.aacceptPostMessages.includes(method)){
+                if(this.methodIdMap[ method ] || Ext.aacceptPostMessages.includes(method)){
                     this[ method ]( params );
                     clearTimeout(this.methodIdMap[ method ]);
                     delete this.methodIdMap[ method ];
@@ -124,7 +127,7 @@ class ServiceWorker {
 
     postMessage(method, params = {}){
         const requestObj = this.getRequestObj( method, params );
-        const methodId = setTimeout( () => this.handleErrorMessage(method), ServiceWorker.activeMethodSecond);
+        const methodId = setTimeout( () => this.handleErrorMessage(method), Ext.activeMethodSecond);
         this.methodIdMap[method] = methodId;
         this.iframe.contentWindow.postMessage(requestObj, this.talknUrl);
     }
@@ -134,30 +137,30 @@ class ServiceWorker {
             switch(method){
             case 'bootExtension':
                 console.log("FAULT");
-                new ServiceWorker(true);
+                new Ext(true);
                 break;
             }
         }
     }
 
-    toggleIframe(params){
-        const iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
-        const talknNotifId = sessionStorage.getItem(ServiceWorker.talknNotifId);
+    toggleExt(params){
+        const iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
+        const talknNotifId = sessionStorage.getItem(Ext.talknNotifId);
 
         if(talknNotifId === "null"){
-            if( iframe.style.height !== ServiceWorker.iframeOpenHeight ){
+            if( iframe.style.height !== Ext.iframeOpenHeight ){
                 iframe.style.transition = "600ms";
-                iframe.style.height = ServiceWorker.iframeOpenHeight;
+                iframe.style.height = Ext.iframeOpenHeight;
             }else{
                 iframe.style.transition = "600ms";
-                iframe.style.height = ServiceWorker.iframeCloseHeight;
+                iframe.style.height = Ext.iframeCloseHeight;
             }
         }else{
             clearTimeout( talknNotifId );
-            sessionStorage.setItem(ServiceWorker.talknNotifId, null);
+            sessionStorage.setItem(Ext.talknNotifId, null);
             this.postMessage("closeNotif");
             iframe.style.transition = "600ms";
-            iframe.style.height = ServiceWorker.iframeOpenHeight;
+            iframe.style.height = Ext.iframeOpenHeight;
         }
     }
 
@@ -167,26 +170,26 @@ class ServiceWorker {
     }
 
     openNotif(params){
-        const iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+        const iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
         iframe.style.transition = "0ms";
-        iframe.style.height = ServiceWorker.iframeCloseNotifHeight;
+        iframe.style.height = Ext.iframeCloseNotifHeight;
         this.postMessage("openNotif");
-        let talknNotifId = sessionStorage.getItem(ServiceWorker.talknNotifId);
+        let talknNotifId = sessionStorage.getItem(Ext.talknNotifId);
         if(talknNotifId){
             clearTimeout( talknNotifId );
         }
 
         talknNotifId = setTimeout( this.closeNotif, params.transition );
-        sessionStorage.setItem(ServiceWorker.talknNotifId, talknNotifId);
+        sessionStorage.setItem(Ext.talknNotifId, talknNotifId);
     }
 
     closeNotif(params){
-        let talknNotifId = sessionStorage.getItem(ServiceWorker.talknNotifId);
+        let talknNotifId = sessionStorage.getItem(Ext.talknNotifId);
         clearTimeout( talknNotifId );
-        sessionStorage.setItem(ServiceWorker.talknNotifId, null);
-        const iframe = document.querySelector(`iframe#${ServiceWorker.APP_NAME}Extension`);
+        sessionStorage.setItem(Ext.talknNotifId, null);
+        const iframe = document.querySelector(`iframe#${Ext.APP_NAME}Extension`);
         iframe.style.transition = "0ms";
-        iframe.style.height = ServiceWorker.iframeCloseHeight;
+        iframe.style.height = Ext.iframeCloseHeight;
         this.postMessage("closeNotif");
     }
 
@@ -224,7 +227,7 @@ class ServiceWorker {
 
     getRequestObj(method, params = {}){
         return {
-            type: ServiceWorker.APP_NAME,
+            type: Ext.APP_NAME,
             url: location.href,
             href: location.href,
             method: method,
@@ -233,4 +236,4 @@ class ServiceWorker {
         };
     }
 }
-const c = new ServiceWorker();
+const e = new Ext();
