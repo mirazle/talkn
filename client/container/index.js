f490dc40cdd3eba926fd136a819b20453446feae
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-05-14 19:25:18 +0900

    Execute deploy command

--- client/container/index.js
+++ client/container/index.js
@@ -60,8 +60,9 @@ class Container extends Component {
         "ON_CLICK_OPEN_LOCK_MENU",
         "SERVER_TO_CLIENT[EMIT]:getMore",
         "ON_CLICK_MULTISTREAM",
-        "RESIZE_END_WINDOW"
 */
+        "RESIZE_END_WINDOW"
+
       ].includes( actionLog[0] );
     default: 
       return true;
