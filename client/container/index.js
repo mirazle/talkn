2baefcd17bbd8b1cb01dc35e578f2430de1872a5
hmiyazaki <hmiyazaki@hmiyazakinoMacBook-Pro.local>
2019-05-14 22:02:01 +0900

    Execute deploy command

--- client/container/index.js
+++ client/container/index.js
@@ -55,8 +55,8 @@ class Container extends Component {
     const {app, actionLog} = props.state;
     switch( app.type ){
     case define.APP_TYPES.EXTENSION:
-      return true;
-/*
+//      return true;
+
       return [
         "SERVER_TO_CLIENT[BROADCAST]:find",
         "ON_CLICK_TOGGLE_MAIN",
@@ -68,7 +68,7 @@ class Container extends Component {
 //        "RESIZE_END_WINDOW"
 
       ].includes( actionLog[0] );
-*/
+
     default: 
       return true;
     }
@@ -343,7 +343,7 @@ class Container extends Component {
         console.log( "isTransition = " + app.isTransition + " @");
         console.log("=============================================");
       }
-        
+
       const Notifs = this.getNotifs( props );
       return (
         <span data-component-name={this.constructor.name} style={ style.container.self }>
