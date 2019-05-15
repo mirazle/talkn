010bc1bf9c9be95d8250d5cdba3399640a82f259
hmiyazaki <hmiyazaki@hm.local>
2019-05-15 12:22:38 +0900

    Execute deploy command

--- client/container/index.js
+++ client/container/index.js
@@ -288,6 +288,7 @@ class Container extends Component {
       return (
         <span data-component-name={this.constructor.name} style={ style.container.self }>
           <span data-component-name="fixedComponents">
+            <div></div>
             <PostsFooter {...props} />
           </span>
         </span>
@@ -299,7 +300,7 @@ class Container extends Component {
       return (
         <span data-component-name={this.constructor.name} style={ style.container.self }>
           <span data-component-name="fixedComponents">
-            <div>@@@</div>
+            { Notifs }
             <PostsFooter {...props} />
           </span>
         </span>
