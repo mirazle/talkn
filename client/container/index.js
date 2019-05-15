49cec3587e0335d92d5598d1384162eb717ea57f
hmiyazaki <hmiyazaki@hm.local>
2019-05-15 11:29:08 +0900

    Execute deploy command

--- client/container/index.js
+++ client/container/index.js
@@ -301,11 +301,12 @@ class Container extends Component {
         console.log( "isTransition = " + app.isTransition + " @");
         console.log("=============================================");
       }
-
+      const Notifs = this.getNotifs( props );
       return (
         <span data-component-name={this.constructor.name} style={ style.container.self }>
           <Style {...props} />
           <span data-component-name="fixedComponents">
+          { Notifs }
             <PostsFooter {...props} />
           </span>
         </span>
@@ -349,6 +350,7 @@ class Container extends Component {
         <span data-component-name={this.constructor.name} style={ style.container.self }>
           <Style {...props} />
           <span data-component-name="fixedComponents">
+            { Notifs }
             <PostsFooter {...props} />
           </span>
         </span>
