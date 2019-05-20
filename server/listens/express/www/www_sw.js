470e91ca7258584c8fa82d55094b7e84a456cede
hmiyazaki <hmiyazaki@hm.local>
2019-05-20 18:54:01 +0900

    Execute deploy command

--- server/listens/express/www/www_sw.js
+++ server/listens/express/www/www_sw.js
@@ -55,23 +55,9 @@ self.addEventListener('fetch', function(event) {
             if(response){
                 return response;
             }else{
-
-                console.log("@@@@@@@@@@@@@@@@@@@@@@@ " + event.request.destination);
-                console.log(event.request.bodyUsed);
-                console.log(event.request.cache);
-                console.log(event.request.credentials);
-                console.log(event.request.destination);
-                console.log(event.request.integrity);
-                console.log(event.request.isHistoryNavigation);
-                console.log(event.request.keepalive);
-                console.log(event.request.method);
-                console.log(event.request.mode);
-                console.log(event.request.redirect);
-                console.log(event.request.referrer);
-                console.log(event.request.referrerPolicy);
-                console.log(event.request.url);
-
-                return fetch ? fetch(event.request, { mode: 'no-cors' }) : () => {};
+                if( event.request.destination !== "unknown" ){
+                    fetch(event.request, { mode: 'no-cors' });
+                }
             }
         })
     );
