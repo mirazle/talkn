7406ab8dad93d864dcb700fb46277e6ec8282585
hmiyazaki <hmiyazaki@hm.local>
2019-05-21 21:44:29 +0900

    Execute deploy command

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ yarn run build
 echo "@@@@@@ RESOLVE EXTENSION"
 cp server/listens/express/client/talkn.client.js extention/talkn.client.js
 cp server/listens/express/ext/ext.js extention/ext.js
-echo "@@@@@@ COMPRESSIONS(SEREVR&EXTENSION)"
+echo "@@@@@@ COMPRESSIONS(EXTENSION)"
 zip -r chromeExtension extention/*
 echo "@@@@@@ GIT PUSH"
 git add ./*
