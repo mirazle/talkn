54983bfff52f7ffd3e9db31dc2870c41c0363e8a
hmiyazaki <hmiyazaki@hm.local>
2019-05-22 17:13:31 +0900

    Execute deploy command

--- server/logics/Geolite.js
+++ server/logics/Geolite.js
@@ -1,56 +1,59 @@
 export default class Geolite {
   static getLanguage(req){
+    let language = "en";
 
-    //const splitedAccesptLanguage = req.headers['accept-language'].split(",");
-    //return splitedAccesptLanguage[ 0 ];
-    if( req.headers['accept-language'].indexOf("ja") === 0 ){
-      return "ja";      
-    }else if( req.headers['accept-language'].indexOf("zh-TW") === 0 ){
-      return "zh-TW";
-    }else if( req.headers['accept-language'].indexOf("zh") === 0 ){
-      return "zh";
-    }else if( req.headers['accept-language'].indexOf("de") === 0 ){
-      return "de";
-    }else if( req.headers['accept-language'].indexOf("fr") === 0 ){
-      return "fr";
-    }else if( req.headers['accept-language'].indexOf("hi") === 0 ){
-      return "hi";
-    }else if( req.headers['accept-language'].indexOf("pt") === 0 ){
-      return "pt";
-    }else if( req.headers['accept-language'].indexOf("it") === 0 ){
-      return "it";
-    }else if( req.headers['accept-language'].indexOf("ru") === 0 ){
-      return "ru";
-    }else if( req.headers['accept-language'].indexOf("ko") === 0 ){
-      return "ko";
-    }else if( req.headers['accept-language'].indexOf("es") === 0 ){
-      return "es";
-    }else if( req.headers['accept-language'].indexOf("id") === 0 ){
-      return "id";
-    }else if( req.headers['accept-language'].indexOf("tr") === 0 ){
-      return "tr";
-    }else if( req.headers['accept-language'].indexOf("nl") === 0 ){
-      return "nl";
-    }else if( req.headers['accept-language'].indexOf("ar") === 0 ){
-      return "ar";
-    }else if( req.headers['accept-language'].indexOf("pl") === 0 ){
-      return "pl";
-    }else if( req.headers['accept-language'].indexOf("sv") === 0 ){
-      return "sv";
-    }else if( req.headers['accept-language'].indexOf("th") === 0 ){
-      return "th";
-    }else if( req.headers['accept-language'].indexOf("fa") === 0 ){
-      return "fa";
-    }else if( req.headers['accept-language'].indexOf("nn") === 0 ){
-      return "nn";
-    }else if( req.headers['accept-language'].indexOf("ga") === 0 ){
-      return "ga";
-    }else if( req.headers['accept-language'].indexOf("he") === 0 ){
-      return "he";
-    }else if( req.headers['accept-language'].indexOf("mr") === 0 ){
-      return "mr";
-    }else{
-      return "en";
+    if(req.headers && req.headers['accept-language']){
+
+      if( req.headers['accept-language'].indexOf("ja") === 0 ){
+        language = "ja";      
+      }else if( req.headers['accept-language'].indexOf("zh-TW") === 0 ){
+        language = "zh-TW";
+      }else if( req.headers['accept-language'].indexOf("zh") === 0 ){
+        language = "zh";
+      }else if( req.headers['accept-language'].indexOf("de") === 0 ){
+        language = "de";
+      }else if( req.headers['accept-language'].indexOf("fr") === 0 ){
+        language = "fr";
+      }else if( req.headers['accept-language'].indexOf("hi") === 0 ){
+        language = "hi";
+      }else if( req.headers['accept-language'].indexOf("pt") === 0 ){
+        language = "pt";
+      }else if( req.headers['accept-language'].indexOf("it") === 0 ){
+        language = "it";
+      }else if( req.headers['accept-language'].indexOf("ru") === 0 ){
+        language = "ru";
+      }else if( req.headers['accept-language'].indexOf("ko") === 0 ){
+        language = "ko";
+      }else if( req.headers['accept-language'].indexOf("es") === 0 ){
+        language = "es";
+      }else if( req.headers['accept-language'].indexOf("id") === 0 ){
+        language = "id";
+      }else if( req.headers['accept-language'].indexOf("tr") === 0 ){
+        language = "tr";
+      }else if( req.headers['accept-language'].indexOf("nl") === 0 ){
+        language = "nl";
+      }else if( req.headers['accept-language'].indexOf("ar") === 0 ){
+        language = "ar";
+      }else if( req.headers['accept-language'].indexOf("pl") === 0 ){
+        language = "pl";
+      }else if( req.headers['accept-language'].indexOf("sv") === 0 ){
+        language = "sv";
+      }else if( req.headers['accept-language'].indexOf("th") === 0 ){
+        language = "th";
+      }else if( req.headers['accept-language'].indexOf("fa") === 0 ){
+        language = "fa";
+      }else if( req.headers['accept-language'].indexOf("nn") === 0 ){
+        language = "nn";
+      }else if( req.headers['accept-language'].indexOf("ga") === 0 ){
+        language = "ga";
+      }else if( req.headers['accept-language'].indexOf("he") === 0 ){
+        language = "he";
+      }else if( req.headers['accept-language'].indexOf("mr") === 0 ){
+        language = "mr";
+      }else{
+        language = "en";
+      }
     }
+    return language;
   }
 }
