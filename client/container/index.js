103cb346944460ed017f1e95b23e0f104779d6bc
hmiyazaki <hmiyazaki@hm.local>
2019-05-25 19:33:30 +0900

    Execute deploy command

--- client/container/index.js
+++ client/container/index.js
@@ -297,7 +297,7 @@ class Container extends Component {
         </div>
         <span data-component-name="fixedComponents">
           { Notifs }
-          <PostsFooter {...props} debug={app.isDispPosts + " " + app.isOpenPosts} />
+          <PostsFooter {...props} debug={""} />
           <InnerNotif {...this.props}/>
         </span>
       </span>
