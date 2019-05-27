5f3a436f75c62dcc7d46cae701969ed926afd052
hmiyazaki <hmiyazaki@hm.local>
2019-05-27 13:17:21 +0900

    Execute deploy command

--- extention/ext.js
+++ extention/ext.js
@@ -194,7 +194,12 @@ class Ext {
         const requestObj = this.getRequestObj( method, params );
         const methodId = setTimeout( () => this.handleErrorMessage(method), Ext.activeMethodSecond);
         this.methodIdMap[method] = methodId;
-        this.iframe.contentWindow.postMessage(requestObj, this.talknUrl);
+        if(this.iframe.contentWindow.postMessage){
+            this.iframe.contentWindow.postMessage(requestObj, this.talknUrl);
+        }else{
+            console.warn( "NULL postMessage METHOD: " + method + " URL: " + this.talknUrl);
+            console.warn( requestObj );
+        }
     }
 
     handleErrorMessage(method){
