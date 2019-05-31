6ba6839ec3c8623a66da25152fd10598c1c93bb0
hmiyazaki <hmiyazaki@hm.local>
2019-05-31 12:15:38 +0900

    Execute deploy command

--- client/components/PostsFooter.js
+++ client/components/PostsFooter.js
@@ -92,6 +92,7 @@ export default class PostsFooter extends Component {
       document.querySelector("button").textContent = "Focus " + window.scrollY;
 
 
+      talknAPI.extension("optimizeScrollAndHeight");
       talknAPI.extension("setScrollAndHeight");
 
       setTimeout( () => {
