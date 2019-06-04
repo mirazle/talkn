52d94283074e2e1efe915111eab751ee43d763e5
hmiyazaki <hmiyazaki@hm.local>
2019-06-04 22:40:45 +0900

    cleanApp

--- schemas/state/MenuIndex.js
+++
@@ -1,8 +0,0 @@
-import Schema from '~/common/schemas/Schema';
-
-export default class MenuIndex extends Schema{
-  constructor( params = [] ){
-    super();
-    return this.create( params );
-  }
-}
--- schemas/state/PostMulti.js
+++
@@ -1,6 +0,0 @@
-import Schema from '~/common/schemas/Schema';
-export default class PostMulti {
-  constructor(){
-    return [];
-  }
-}
