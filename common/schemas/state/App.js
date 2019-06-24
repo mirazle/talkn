f22b4c4cb717c39fdae1dc08588dd6a9c4df4d07
hmiyazaki <hmiyazaki@hm.local>
2019-06-24 10:36:14 +0900

    Execute deploy command

--- common/schemas/state/App.js
+++ common/schemas/state/App.js
@@ -260,7 +260,7 @@ export default class App extends Schema{
       extensionCloseHeight
     } = app;
     const log = false;
-    const al = true;
+    const al = false;
     if(
       extensionMode === App.extensionModeExtBottomLabel ||
       extensionMode === App.extensionModeExtModalLabel 
