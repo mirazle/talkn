4f59c06b2b58d347a008556517bdf5fdab24e683
hmiyazaki <hmiyazaki@hm.local>
2019-07-02 19:25:05 +0900

    Execute deploy command

--- client/style/PostsSupporter.js
+++ client/style/PostsSupporter.js
@@ -3,95 +3,13 @@ import App from '../../common/schemas/state/App';
 import Style from './index';
 import Container from './Container';
 import PostsFooter from './PostsFooter';
-import Menu from './Menu';
 
 export default class PostsSupporter{
 
   static get selfHeight(){ return 45 };
-  static getDisplay( app ){
-    return app.isOpenPostsSupporter ? "flex": "none";
-  }
-  static getWidth( app, addUnit = false ){
-    let width = 0;
-    if(
-      app.extensionMode === App.extensionModeExtBottomLabel ||
-      app.extensionMode === App.extensionModeExtModalLabel 
-    ){
-      width = '100%';
-    }else{
-      switch( app.screenMode ){
-      case App.screenModeUndispLabel : width = '100%';break;
-      case App.screenModeSmallLabel : width = '100%';break;
-      case App.screenModeMiddleLabel : width = Posts.getWidth( app );break;
-      case App.screenModeLargeLabel : width = Posts.getWidth( app );break;
-      }
-    }
-    return addUnit ? Style.trimUnit( width ) : width ;
-  }
-
-  static getLeft( app, addUnit = false ){
-    let left = 0;
-    if( app.extensionMode === App.extensionModeExtBottomLabel ){
-      return 0;
-    }else if(app.extensionMode ===  App.extensionModeExtModalLabel ){
-      return 0;
-    }else{
-      switch( app.screenMode ){
-      case App.screenModeUndispLabel : left = '0px';break;
-      case App.screenModeSmallLabel : left = '0px';break;
-      case App.screenModeMiddleLabel :left = `${Menu.getWidth(app)}`;break;
-      case App.screenModeLargeLabel : left = Menu.getWidth( app );break;
-      }
-    }
-    return addUnit ? Style.trimUnit( left ) : left ;
-  };
-
-  static getBorder( app, addUnit = false ){
-    switch( app.extensionMode ){
-    case App.extensionModeExtBottomLabel:
-      return {borderTop: Container.border, borderRight: Container.border, borderLeft: Container.border};  
-    case App.extensionModeExtModalLabel:
-      return {border: Container.border};
-    default:
-      if( app.includeIframeTag ){
-          return {border: Container.border};
-        }else{
-          if(
-            app.screenMode === App.screenModeUndispLabel ||
-            app.screenMode === App.screenModeSmallLabe
-          ){
-            return {borderTop: Container.border, borderBottom: Container.border} ;
-          }else{
-            return {borderTop: Container.border, borderBottom: Container.border} ;
-          }
-        }
-    }
-  }
-
-  static getBorderRadius( app, addUnit = false ){
-    if( app.extensionMode === App.extensionModeExtBottomLabel ){
-      return app.extensionWidth === "100%" ?
-        "0px 0px 0px 0px" : `${Container.radius} ${Container.radius} 0px 0px`;
-    }else if( app.extensionMode === App.extensionModeExtModalLabel ){
-      return `0px 0px ${Container.radius} ${Container.radius}`;
-    }
-    return 0;
-  }
-
   static getTransform( app ){
-    let transform = 'translate3d( 0px, 0px, 0px )';
-    switch( app.screenMode ){
-    case App.screenModeUndispLabel :
-    case App.screenModeSmallLabel :
-      transform = app.isOpenMenu ? 'translate3d( 0%, 0px, 0px )' : 'translate3d( 0px, 0px, 0px )';
-      break;
-    case App.screenModeMiddleLabel :
-      transform = app.isOpenDetail ? `translate3d( 0px ,0px, 0px )` : 'translate3d( 0px ,0px, 0px )';
-      break;
-    case App.screenModeLargeLabel : transform = 'translate3d( 0px ,0px, 0px )';
-      break;
-    }
-    return transform ;
+    return app.isOpenPostsSupporter ? 
+    `translate3d( 0px, -${ PostsSupporter.selfHeight + PostsFooter.selfHeight }px, 0px )` : 'translate3d( 0px, 0px, 0px )';
   }
 
   constructor( params ){
@@ -104,11 +22,10 @@ export default class PostsSupporter{
   }
 
   static getSelf( {app} ){
-    const display = PostsSupporter.getDisplay(app);
     const layout = Style.getLayoutFlex({
-      display,
+      display: "flex",
       position: "fixed",
-      bottom: `${PostsFooter.selfHeight}px`,
+      bottom: `-${ PostsSupporter.selfHeight }px`,
       left: PostsFooter.getLeft( app ),
       height: PostsFooter.selfHeight,
       width: PostsFooter.getWidth( app ),
@@ -125,6 +42,7 @@ export default class PostsSupporter{
     });
     const content = {};
     const animation = Style.getAnimationBase({
+      transition: Container.getTransitionFirstOn( app ),
       transform: PostsSupporter.getTransform( app )
     });
     return Style.get({layout, content, animation});
