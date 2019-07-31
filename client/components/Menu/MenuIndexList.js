9b4ecc76ac7823f925fd5feb1b2286c1bfb2f3dc
hmiyazaki <hmiyazaki@hm.local>
2019-07-31 12:47:39 +0900

    Execute deploy command

--- client/components/Menu/MenuIndexList.js
+++ client/components/Menu/MenuIndexList.js
@@ -214,7 +214,6 @@ export default class MenuIndexList extends Component {
     const baseStyle = style[ styleKey ];
     const dispExt = menuIndexList.findType === Thread.findTypeHtml ?
       null : menuIndexList.findType;
-
     return (
       <li
         data-component-name={"MenuIndexList"}
