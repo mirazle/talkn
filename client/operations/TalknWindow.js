fc52edfefc76aa7eecc699e61ead4ffed229c150
hmiyazaki <hmiyazaki@hm.local>
2019-08-15 15:39:36 +0900

    Execute deploy command

--- client/operations/TalknWindow.js
+++ client/operations/TalknWindow.js
@@ -204,7 +204,6 @@ export default class TalknWindow {
 			case "findPlayConnection" :
 				if( e.data.params.connection && e.data.params.playCnt === 0 ){
 					actionWrap.onClickConnection( e.data.params.connection, false, e.data.method );
-					console.log( e.data.params );
 					TalknMedia.init();
 					const connection = e.data.params.connection;
 					const timeline = storage.getStoragePostsTimeline( connection );
