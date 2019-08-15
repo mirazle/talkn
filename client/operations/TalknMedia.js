aa118e41abbee3c757ba2e1c0b49259706c4f456
hmiyazaki <hmiyazaki@hm.local>
2019-08-15 17:02:05 +0900

    Execute deploy command

--- client/operations/TalknMedia.js
+++ client/operations/TalknMedia.js
@@ -54,7 +54,8 @@ export default class TalknMedia {
 		const currentTime =  Number.MAX_SAFE_INTEGER;
 		const timeline = this.timeline;
 		const length = this.timeline.length;
-
+		console.log("@@@@@@@@@@@@@@@@@@@@@@@@ END");
+		console.log(timeline);
 		for( let i = 0; i < length; i++ ){
 			if( timeline[ i ] && timeline[ i ].currentTime <= currentTime ){
 				talknAPI.nextPostsTimeline([ timeline[ i ] ]);
