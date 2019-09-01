f63fe02d1c10d2a36e302fa0616ea84d075c5905
hmiyazaki <hmiyazaki@hm.local>
2019-09-01 14:28:03 +0900

    Execute deploy command

--- common/schemas/Schema.js
+++ common/schemas/Schema.js
@@ -151,6 +151,7 @@ export default class Schema {
     console.log( validValue );
     */
     if(error){
+      console.warn("BAD: canSet: " + key + " " + validValue );
       return false;
     }else{
       this[ key ] = currentValue;
