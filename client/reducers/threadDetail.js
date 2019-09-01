52848ea165acd8cfb30809974f7699498cb4ce5e
hmiyazaki <hmiyazaki@hm.local>
2019-09-01 16:13:50 +0900

    Execute deploy command

--- client/reducers/threadDetail.js
+++ client/reducers/threadDetail.js
@@ -2,7 +2,13 @@ import Thread from 'common/schemas/state/Thread';
 
 export default ( state = new Thread() , action ) => {
 	if( action.threadDetail ){
+		console.log(state);
 		console.log( action.threadDetail);
+
+		/*
+			マージが失敗しているのが原因でstate.mergeが出来なくなっている
+		*/
+
 		const s = state.merge( action.threadDetail );
 		console.log( s.merge );
 		return s;//state.merge( action.threadDetail );
