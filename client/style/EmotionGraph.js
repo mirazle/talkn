defe92f67f01ccef832cf707f7b20dc14de697ec
hmiyazaki <hmiyazaki@hm.local>
2019-09-12 13:56:05 +0900

    Execute deploy command

--- client/style/EmotionGraph.js
+++ client/style/EmotionGraph.js
@@ -5,12 +5,15 @@ export default class EmotionGraph {
 
   static get datasetsBase(){
     return {
-        backgroundColor: 'rgba(124, 166, 158, 0.2)',
-        borderColor: 'rgba(124, 166, 158, 0.8)',
-        pointBackgroundColor: 'rgba(124, 166, 158, 1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(124, 166, 158,1)',
+        backgroundColor: 'rgba(240, 100, 195, 0.2 )',
+        borderCapStyle: "square",
+        borderColor: 'rgba(240, 100, 195, 0.8 )',
+        borderWidth: "4",
+        pointBackgroundColor: 'rgba(240, 100, 195, 0.8 )',
+        pointBorderColor: 'rgba(240, 100, 195, 0 )',
+        pointHoverBackgroundColor: 'rgba(255, 255, 255, 0.8 )',
+        pointHoverBorderColor: 'rgba(240, 100, 195, 0.8 )',
+        pointHoverBorderWidth: "4",
         data: []
       };
   }
@@ -19,14 +22,47 @@ export default class EmotionGraph {
     return {
       responsive: true,
       responsiveAnimationDuration: 0,
+      elements:{
+        arc: {
+          backgroundColor: "red",
+          borderAlign: "left",
+          borderColor: "red",
+          borderWidth:  "10"
+        },
+        line: {
+          tension: 0.01,//0.000001
+          backgroundColor: "rgba(79, 174, 159, 0.6)",
+          borderWidth: "10",
+          borderColor: "rgba(79, 174, 159, 0.6)",
+          borderCapStyle: "square",
+          borderDash: []
+        }
+      },
+      // グラフ外部の指標のタイトル
       legend: {
         display: false
       },
       tooltips: {
-        enabled: true
+        enabled: true,
+        intersect: true,
+        backgroundColor: 'rgba(240, 100, 195, 0.7 )',
+        callbacks: {
+          label: (tooltipItem, data) => {
+              let label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+              if (label) {
+                  label += ': ';
+              }
+              label += Math.round(tooltipItem.yLabel * 100) / 100;
+              console.log( label );
+              return label;
+          }
+        }
       },
       scale: {
         ticks: {
+            fontSize: 17,
+            fontColor: Container.fontBaseRGB,
             beginAtZero: true,
             max: 5,
             min: 0,
