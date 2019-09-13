62cec4c6a7556b4b7ec68d21b126996ded0193f8
hmiyazaki <hmiyazaki@hm.local>
2019-09-13 12:59:43 +0900

    Execute deploy command

--- client/reducers/threadDetail.js
+++ client/reducers/threadDetail.js
@@ -2,5 +2,6 @@ import Thread from 'common/schemas/state/Thread';
 
 export default ( state = new Thread() , action ) => {
 	console.log(action.threadDetail);
+	console.log( state );
 	return action.threadDetail ? state.merge( action.threadDetail ) : state ;
 };
