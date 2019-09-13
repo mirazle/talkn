e71605ab1ca5daba0f79277327c708293fe64686
hmiyazaki <hmiyazaki@hm.local>
2019-09-13 16:13:55 +0900

    Execute deploy command

--- client/reducers/threadDetail.js
+++ client/reducers/threadDetail.js
@@ -2,9 +2,9 @@ import Thread from 'common/schemas/state/Thread';
 
 export default ( state = new Thread() , action ) => {
 	if( action.threadDetail ){
-		console.log( "@@@@ REDUCER " );
+		console.log( "@@@@ REDUCER @@@@" );
 		console.log(action.threadDetail.hasSlash);
-		console.log( state .hasSlash);
+		console.log( state.hasSlash);
 	}
 	return action.threadDetail ? state.merge( action.threadDetail ) : state ;
 };
