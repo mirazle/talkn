ffc79db64ac6fab0fcb87f0c30120f21f2296c00
mirazle <mirazle2069@gmail.com>
2019-09-27 16:37:26 +0900

    Execute deploy command

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,43 +2,52 @@ module.exports = {
   mode: process.env.WEBPACK_ENV,
   context: __dirname + "/src/",
   entry: {
-    javascript: __dirname + "/src/talkn.client.js",
+    javascript: __dirname + "/src/talkn.client.js"
   },
 
   output: {
     path: __dirname + "/../server/listens/express/client/",
-    filename: "talkn.client.js",
+    filename: "talkn.client.js"
   },
 
   module: {
     rules: [
       {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loaders: ["ts-loader"]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ["babel-loader"],
+        loaders: ["babel-loader"]
       },
       {
         test: /\.html$/,
-        loader: "file?name=[name].[ext]",
+        loader: "file?name=[name].[ext]"
       },
       {
         test: /\.css$/,
-        loader: "file?name=[name].[ext]",
-      },
-    ],
+        loader: "file?name=[name].[ext]"
+      }
+    ]
   },
-
+  /*
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
+*/
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx", "ts", "tsx"]
   },
 
   devServer: {
     historyApiFallback: true,
-    contentBase: __dirname + "/src/",
+    contentBase: __dirname + "/src/"
   },
 
   performance: {
     hints: false
   },
-  devtool: 'inline-source-map',
-}
+  devtool: "inline-source-map"
+};
