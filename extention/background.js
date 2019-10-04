daaf883beaa87f2b5d932970c7e4150844b302a3
mirazle <mirazle2069@gmail.com>
2019-10-04 16:39:51 +0900

    test

--- extention/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener( (request, sender, callback) => {
-    const options = localStorage.getItem("talknExtensionOptions");
-    console.log( options );
-    callback(options);
-});
