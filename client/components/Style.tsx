7e923cbfec2d2b9a78bafa9db4b30fab8712fdb3
mirazle <mirazle2069@gmail.com>
2020-01-05 16:07:40 +0900

    Execute deploy command

--- client/components/Style.tsx
+++ client/components/Style.tsx
@@ -26,9 +26,9 @@ export default class Style extends Component<Props> {
             color: rgb(170, 170, 170);
           }
           @keyframes LogoWrap1 {
-            0%   { transform: scale(1); opacity: 1.0; }
-            50%   { transform: scale(1); opacity: 1.0; }
-            100%   { transform: scale(1); opacity: 1.0; }
+            0%   { transform: scale(1); opacity: 1.0; box-shadow: 0px 0px 1px 0px rgba( 79, 174, 159, 0.4 ), 80px 80px 80px 0px rgba( 79, 174, 159, 0.4 ) inset; }
+            50%   { transform: scale(1); opacity: 1.0; box-shadow: 0px 20px 60px 0px rgba( 79, 174, 159, 0.6 ), 80px 80px 80px 0px rgba( 79, 174, 159, 0.4 ) inset;}
+            100%   { transform: scale(1); opacity: 1.0; box-shadow: 0px 0px 1px 0px rgba( 79, 174, 159, 0.4 ), 80px 80px 80px 0px rgba( 79, 174, 159, 0.4 ) inset;}
           }
           @keyframes Logo {
             0%   { transform: scale(0.95) translate3d(0px, 0px, 0px); opacity: 0.98; }
@@ -51,12 +51,12 @@ export default class Style extends Component<Props> {
             position: absolute;
             top: 35vh;
             animation-name: LogoWrap1;
-            animation-duration: 500ms;
+            animation-duration: 2000ms;
             animation-iteration-count: infinite;
             width: 80px;
             height: 80px;
             border-radius: 10000px;
-            box-shadow: 0px 0px 15px 1px rgba( 150, 150, 150, 0.4 ), 80px 80px 80px 0px rgba( 79,174,159, 0.22 ) inset;
+            box-shadow: 0px 0px 30px 0px rgba( 100, 100, 100, 0.6 ), 80px 80px 80px 0px rgba( 79, 174, 159, 0.4 ) inset;
             transform: tlanslate3d( 0px, -20px, 0px );
           }
           .Logo {
