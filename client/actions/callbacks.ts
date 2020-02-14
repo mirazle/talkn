87b1c2ba9bf06583b17e9df7ea359d53775ee522
mirazle <mirazle2069@gmail.com>
2020-02-14 20:10:38 +0900

    Execute deploy command

--- client/actions/callbacks.ts
+++ client/actions/callbacks.ts
@@ -1,14 +1,6 @@
 export default {
-  scrollThread: () => {
-    return { type: "SCROLL_THREAD" };
-  },
   endAnimateScrollTo: () => {
     return { type: "END_ANIMATE_SCROLL_TO" };
-  },
-  createNotif: () => {
-    return {
-      type: "CREATE_NOTIF"
-    };
   },
   updatePostsHeight: postsHeight => {
     return {
