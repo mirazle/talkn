85e6ee83f049eafbacc812176bf81212008dde16
mirazle <mirazle2069@gmail.com>
2020-06-05 19:49:56 +0900

    Execute deploy command

--- server/listens/express/portal/portal_sw.js
+++ server/listens/express/portal/portal_sw.js
@@ -2,15 +2,15 @@ const cacheName = "pwa-talkn";
 const log = false;
 const filesToCache = ["/", "/index.ejs"];
 
-if (log) console.log("SW");
+if (log) console.log("SW OK");
 
 // Install Service Worker
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   if (log) console.log("[INSTALL]Service Worker: Installing.... " + cacheName);
 
   event.waitUntil(
     // Open the Cache
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       if (log) console.log("[INSTALLED]Service Worker: Caching App Shell at the moment...... " + cacheName);
 
       // Add Files to the Cache
@@ -20,15 +20,15 @@ self.addEventListener("install", function(event) {
 });
 
 // Fired when the Service Worker starts up
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
   if (log) console.log("Service Worker: Activating....");
   var cacheWhitelist = ["dependencies-cache-**v1**", "dependencies-2-cache-**v1**"]; // Version for your cache list
   if (log) console.log("A");
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       if (log) console.log("B");
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (log) console.log("C");
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             if (log) console.log("D");
@@ -41,14 +41,14 @@ self.addEventListener("activate", function(event) {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   if (log) console.log("Service Worker: Fetch", event.request.url);
   if (log) console.log("Url", event.request.url);
 
   // Https request image is error and undisplay.
   if (event.request.url.indexOf("https:") === 0) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
+      caches.match(event.request).then(function (response) {
         console;
         if (response) {
           return response;
