e20379b226fe42202701812472bf930a074ffd90
mirazle <mirazle2069@gmail.com>
2020-06-05 19:46:20 +0900

    Execute deploy command

--- server/listens/express/index.ts
+++ server/listens/express/index.ts
@@ -55,6 +55,7 @@ class Express {
 
   routingHttps(req, res, next) {
     let language = "en";
+    console.log(req.headers.host);
     switch (req.headers.host) {
       case conf.apiURL:
         if (req.url === `/v${conf.apiVer}`) {
