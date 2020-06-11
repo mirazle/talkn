810c8ee9aa67c6b4df4e1ff4e9c216d67c45cb0e
mirazle <mirazle2069@gmail.com>
2020-06-11 16:03:28 +0900

    update

--- common/define.ts
+++ common/define.ts
@@ -2,7 +2,7 @@ const define: any = {
   PRODUCTION: "production",
   LOCALHOST: "localhost",
   DEVELOPMENT: "development",
-  PRODUCTION_IP: "ip-172-26-12-159",
+  PRODUCTION_IP: "ip-172-26-12-159.ec2.internal",
   PRODUCTION_DOMAIN: "talkn.io",
   DEVELOPMENT_DOMAIN: "localhost",
   PORTS: {
