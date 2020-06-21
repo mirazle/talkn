00904ae84c1bcb574d7fcd7f71dbae99ec2a32f2
mirazle <mirazle2069@gmail.com>
2020-06-21 15:11:14 +0900

    Execute deploy command

--- api/reducers/thread.ts
+++ api/reducers/thread.ts
@@ -1,6 +1,6 @@
 import Thread from "api/store/Thread";
 
 export default (state = new Thread({}, {}, {}), action) => {
-  console.log(state);
+  console.log(state.ch);
   return action.thread ? state.merge(action.thread) : state;
 };
