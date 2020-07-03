32e0ba444d24657830c52af7a6343a1b3cd5b59b
mirazle <mirazle2069@gmail.com>
2020-07-03 15:18:36 +0900

    Execute deploy command

--- client/components/Links.tsx
+++ client/components/Links.tsx
@@ -146,7 +146,7 @@ export default class Links extends TalknComponent<LinksProps, LinksState> {
   }
 
   componentDidUpdate(props) {
-    if (props.state.actionLog[0] === "API_TO_CLIENT[EMIT]:fetchPosts") {
+    if (props.state.actionLog[0] === "API_TO_CLIENT[EMIT]:tune") {
       this.setLinkContents();
     }
   }
