6463c8db17ff87416b593a54409c55d268f89518
mirazle <mirazle2069@gmail.com>
2020-07-04 20:02:20 +0900

    Execute deploy command

--- client/style/common/index.ts
+++ client/style/common/index.ts
@@ -1,2 +1,2 @@
 import { StyleObject } from "client/style";
-export const LabelStyle: StyleObject = { maxWidth: 0, maxHeight: 0 };
+export const LabelStyle: StyleObject = { maxWidth: 0, maxHeight: 0, opacity: 0 };
