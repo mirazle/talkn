eddcd717bb08f0f7f08ff08941b781f027c7149c
mirazle <mirazle2069@gmail.com>
2020-08-11 18:18:20 +0900

    Execute deploy command

--- server/listens/express/extension/talkn.ext.js
+++ server/listens/express/extension/talkn.ext.js
@@ -215,9 +215,10 @@ class MediaServer {
             this.setStatus(MediaServer.STATUS_STANBY, `searched video ${iFrameId}`);
           }
         }
-        console.log(this.audios.length + " > 0 && " + iframeHasAudio);
+
         if (this.audios.length > 0 && iframeHasAudio) {
           isHandleEvents = handleEventsWrap("audios");
+          console.log(this.audios.length + " > 0 && " + iframeHasAudio + " " + isHandleEvents);
           if (isHandleEvents) {
             this.setStatus(MediaServer.STATUS_STANBY, `searched audio ${iFrameId}`);
           }
