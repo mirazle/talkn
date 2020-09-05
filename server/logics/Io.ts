222abb77c54d2805ad874665bdb04859908a78cd
mirazle <mirazle2069@gmail.com>
2020-09-05 14:52:23 +0900

    Execute deploy command

--- server/logics/Io.ts
+++ server/logics/Io.ts
@@ -24,7 +24,9 @@ export default class Io {
       setting,
     });
     const responseBroadcastState = Sequence.getResponseState("Broadcast", requestState, { thread });
+    console.log("EMIT! ME");
     this.io.emit(ioUser, Sequence.CATCH_ME_KEY, responseEmitState);
+    console.log("BROADCAST! " + responseBroadcastState.thread.ch);
     this.io.broadcast(responseBroadcastState.thread.ch, responseBroadcastState);
   }
 
