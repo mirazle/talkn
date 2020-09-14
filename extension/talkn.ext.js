ceebe2b1cbc975ffb6b3fe04a171d953fbfead41
mirazle <mirazle2069@gmail.com>
2020-09-14 19:57:38 +0900

    Execute deploy command

--- extension/talkn.ext.js
+++ extension/talkn.ext.js
@@ -222,7 +222,7 @@ class MediaServer {
 
       if (this.searchingCnt < this.maxSearchingCnt) {
         if (this.videos.length > 0 && iframeHasVideo) {
-          isHandleEvent = handleEventsWrap("videos");
+          isHandleEvents = handleEventsWrap("videos");
           if (isHandleEvents) {
             this.setStatus(MediaServer.STATUS_STANBY, `searched video ${iFrameId}`);
           }
