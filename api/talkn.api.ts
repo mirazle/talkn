e10103ddba5de3693ce9b471ae45d8d29274449e
mirazle <mirazle2069@gmail.com>
2021-01-15 23:44:43 +0900

    Execute deploy command

--- api/talkn.api.ts
+++ api/talkn.api.ts
@@ -9,7 +9,7 @@ declare global {
   }
 }
 const script = document.currentScript;
-console.log(script);
+console.log(script.getAttribute('asyn'));
 const isPureApi = Boolean(script.getAttribute("src").indexOf(define.SUB_DOMAINS.BANNER) === -1);
 const bootTalknApi = () => {
   const talknWindow = new Window(define.APP_TYPES.API);
