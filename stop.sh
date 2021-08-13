9999d16460b72aa4efe5b0325ab1f197f0d6ce8a
mirazle <mirazle2069@gmail.com>
2021-08-13 18:56:23 +0900

    Execute deploy command

--- stop.sh
+++ stop.sh
@@ -1,16 +1,2 @@
 echo "@@@@@@ STOP PROCCESS"
-forever stopall
-ps -x | grep node | awk '{print $1}' > p.txt
-
-cat p.txt | while read pid
-do
-    kill -9 $pid
-done
-rm p.txt
-echo "@@@@@@ YARN INSTALL"
-yarn install
-echo "@@@@@@ YARN RUN PROD"
-yarn run prod
-echo "@@@@@@ PROCCESS LIST"
-cd ../
-ps -aux | grep node
+ps -aux | grep node | grep -v grep | awk '{ print "kill -9", $2 }' | sh
