55629187c7d5410707d611c0542c991159c55583
mirazle <mirazle2069@gmail.com>
2021-09-04 18:05:17 +0900

    Execute deploy command

--- server/src/listens/express/index.ts
+++ server/src/listens/express/index.ts
@@ -184,11 +184,12 @@ class Express {
 
         // No Assests Url
         if (`/${req.originalUrl}/` !== conf.assetsPath) {
+          /*
           if (req.originalUrl === '/' && !req.headers.referer) {
             res.redirect(`//${conf.wwwURL}`);
             return true;
           }
-
+*/
           if (req.originalUrl.indexOf('/https:/') >= 0 || req.originalUrl.indexOf('/http:/') >= 0) {
             const redirectUrl = req.originalUrl.replace('/https:/', '').replace('/http:/', '');
             res.redirect(redirectUrl);
