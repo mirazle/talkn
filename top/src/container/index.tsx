693b3945a53ebe51bf2ee76888f874fd1ceb3568
mirazle <mirazle2069@gmail.com>
2021-10-03 19:49:25 +0900

    save

--- top/src/container/index.tsx
+++ top/src/container/index.tsx
@@ -182,6 +182,28 @@ const TalknContainer: React.FC<Props> = (props) => {
           <ToTopLayout>
             <ToTop onClick={handleOnClickToTop}>TOP</ToTop>
           </ToTopLayout>
+          <Sns>
+            <Twitter className="twitter">
+              <a
+                href="https://twitter.com/intent/tweet?screen_name=TwitterDev&ref_src=twsrc%5Etfw"
+                className="twitter-mention-button"
+                data-show-count="false">
+                <TwitterIcon />
+                Tweet
+              </a>
+              <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+            </Twitter>
+            <div className="facebook">
+              <div className="fb-share-button" data-href="https://www.talkn.io" data-layout="button_count" data-size="large">
+                <a
+                  target="_blank"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.talkn.io%2F&amp;src=sdkpreparse"
+                  className="fb-xfbml-parse-ignore">
+                  Share
+                </a>
+              </div>
+            </div>
+          </Sns>
           <CopyrightLayout>
             <Copyright>talkn.io</Copyright>
           </CopyrightLayout>
@@ -485,6 +507,31 @@ const ToTop = styled.div`
 `;
 
 const Copyright = styled.div``;
+
+const Sns = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Twitter = styled.div`
+  padding: 5px 10px;
+  margin-right: 10px;
+  background: #1da1f2;
+  border-radius: 5px;
+  font-size: 13px;
+  color: #fff;
+`;
+
+const TwitterIcon = styled.i`
+  display: inline-flex;
+  background: url(data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%2072%2072%22%3E%3Cpath%20fill%3D%22none%22%20d%3D%22M0%200h72v72H0z%22%2F%3E%3Cpath%20class%3D%22icon%22%20fill%3D%22%23fff%22%20d%3D%22M68.812%2015.14c-2.348%201.04-4.87%201.744-7.52%202.06%202.704-1.62%204.78-4.186%205.757-7.243-2.53%201.5-5.33%202.592-8.314%203.176C56.35%2010.59%2052.948%209%2049.182%209c-7.23%200-13.092%205.86-13.092%2013.093%200%201.026.118%202.02.338%202.98C25.543%2024.527%2015.9%2019.318%209.44%2011.396c-1.125%201.936-1.77%204.184-1.77%206.58%200%204.543%202.312%208.552%205.824%2010.9-2.146-.07-4.165-.658-5.93-1.64-.002.056-.002.11-.002.163%200%206.345%204.513%2011.638%2010.504%2012.84-1.1.298-2.256.457-3.45.457-.845%200-1.666-.078-2.464-.23%201.667%205.2%206.5%208.985%2012.23%209.09-4.482%203.51-10.13%205.605-16.26%205.605-1.055%200-2.096-.06-3.122-.184%205.794%203.717%2012.676%205.882%2020.067%205.882%2024.083%200%2037.25-19.95%2037.25-37.25%200-.565-.013-1.133-.038-1.693%202.558-1.847%204.778-4.15%206.532-6.774z%22%2F%3E%3C%2Fsvg%3E);
+  min-width: 14px;
+  min-height: 14px;
+  width: 14px;
+  height: 14px;
+`;
 
 const getInterviewWidth = (props: InterviewPropsType, InterviewPaddingLeft) => {
   if (props.resumeLayout) {
