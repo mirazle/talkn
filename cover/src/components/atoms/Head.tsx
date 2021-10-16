9785f9ef3b542188b559ecbf19963facd8d73a0f
mirazle <mirazle2069@gmail.com>
2021-10-16 16:47:41 +0900

    save

--- cover/src/components/atoms/Head.tsx
+++ cover/src/components/atoms/Head.tsx
@@ -11,16 +11,20 @@ const Component: React.FC<Props> = ({ thread, serverMetas }) => {
     <Helmet>
       <link rel="icon" href={thread.favicon} />
       <link rel="shortcut icon" href={thread.favicon} />
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       {Object.keys(serverMetas).map((key) => {
         if (key === 'title') {
-          return <title>{serverMetas.title}</title>;
+          return <title key="title">{serverMetas.title}</title>;
+        } else if (key === 'description' || key === 'keywords') {
+          return <meta key={key} name={key} content={serverMetas[key]} />;
         } else if (key === 'og:url') {
           return undefined;
         } else {
           return serverMetas[key] !== '' && <meta key={key} property={key} content={serverMetas[key]} />;
         }
       })}
-      <meta property="og:url" content={`https://cover.talkn.io${thread.ch}`}></meta>;
+      <meta property="og:url" key={'og:url'} content={`https://cover.talkn.io${thread.ch}`}></meta>;
     </Helmet>
   );
 };
