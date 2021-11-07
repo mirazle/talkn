321ce69136b490b7e577e36078b42e234d5a3c8e
mirazle <mirazle2069@gmail.com>
2021-11-07 16:42:21 +0900

    Execute deploy command

--- server/src/logics/Fs.ts
+++ server/src/logics/Fs.ts
@@ -5,13 +5,15 @@ import conf from 'common/conf';
 export default class Fs {
   getInterview(ch, _interviewIndex): any {
     try {
+      const serverPath = `${conf.serverAssetsPath}cover/${ch}interview`;
+      const clientPath = `https://${conf.assetsURL}cover/${ch}interview`;
       const urls = { index: '', interview: '' };
-      urls.index = `${conf.serverAssetsPath}cover/${ch}interview/index.json`;
-      const index = JSON.parse(fs.readFileSync(urls.index, 'utf8'));
+      urls.index = `${clientPath}/index.json`;
+      const index = JSON.parse(fs.readFileSync(`${serverPath}/index.json`, 'utf8'));
       const interviewIndex = _interviewIndex ? _interviewIndex : index.contents.length;
 
-      urls.interview = `${conf.serverAssetsPath}cover/${ch}interview/${interviewIndex}.json`;
-      const interview = JSON.parse(fs.readFileSync(urls.interview, 'utf8'));
+      urls.interview = `${clientPath}/${interviewIndex}.json`;
+      const interview = JSON.parse(fs.readFileSync(`${serverPath}/${interviewIndex}.json`, 'utf8'));
       const css = fs.readFileSync(`${conf.serverAssetsPath}cover/${ch}interview/default.css`, 'utf8');
       return { index, interview, urls, css };
     } catch (err) {
